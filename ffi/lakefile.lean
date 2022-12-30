2ab710f10c5f60ff366a1eede8ec9ce5e155f6d5
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2022-12-30 13:57:52 +0100

    chore: simpler arb lake version checkpoint

--- ffi/lakefile.lean
+++ ffi/lakefile.lean
@@ -2,19 +2,12 @@ import Lake
 open System Lake DSL
 
 def arbDir : FilePath := "/Users/ramonfernandezmir/Documents/PhD-code/verification-tools/arb"
-def flintDir : FilePath := "/Users/ramonfernandezmir/Documents/PhD-code/verification-tools/flint2"
 
 package ffi {
   srcDir := "lean"
   precompileModules := true
-  moreLinkArgs := #[
-    "-L" ++ arbDir.toString,
-    "-I" ++ arbDir.toString,
-    "-larb",
-    "-L" ++ arbDir.toString,
-    "-I" ++ arbDir.toString,
-    "-lflint", "-lmpfr", "-lgmp",
-    "-lstdc++"]
+  moreLinkArgs := #["-larb"]
+  moreLeancArgs := #[]
 }
 
 @[defaultTarget]
@@ -24,25 +17,12 @@ target leanarb.o (pkg : Package) : FilePath := do
   let oFile := pkg.buildDir / "c" / "leanarb.o"
   let srcJob ← inputFile <| pkg.dir / "c" / "leanarb.c"
   let flags := #[
-    "-I" ++ (← getLeanIncludeDir).toString, 
-    "-I" ++ arbDir.toString,
+    "-I" ++ (← getLeanIncludeDir).toString,
     "-O3", "-fPIC"]
   buildFileAfterDep oFile srcJob (extraDepTrace := computeHash flags) fun srcFile => do
-    compileO "leanarb.c" oFile srcFile flags "clang"
+    compileO "leanarb.c" oFile srcFile flags "gcc" -- (← getLeanc)
 
 extern_lib libleanarb (pkg : Package) := do 
-  -- let name := nameToStaticLib "leanffi"
-  -- let libFile := pkg.buildDir / "c" / name
-  -- let arbO ← fetch <| pkg.target ``ffi.o
-  -- let linkJobs := #[arbO]
-  -- let linkArgs := #[
-  --   "-L/usr/local/lib", 
-  --   "-lstdc++",
-  --   "-larb", "-lflint", "-lmpfr", "-lgmp", "-lSystem"]
-  -- let name := libFile.fileName.getD libFile.toString
-  -- buildFileAfterDepArray libFile linkJobs
-  --   (extraDepTrace := computeHash linkArgs) fun links => do
-  --     compileSharedLib name libFile (links.map toString ++ linkArgs) (← getLeanc)
   IO.FS.createDirAll (pkg.buildDir / "lib")
   proc {
     cmd := "cp"
@@ -50,31 +30,10 @@ extern_lib libleanarb (pkg : Package) := do
       (arbDir / "libarb.a").toString,
       (pkg.buildDir / "lib" / "libarb.a").toString ]
   }
-  proc {
-    cmd := "cp"
-    args := #[
-      (flintDir / "libflint.a").toString,
-      (pkg.buildDir / "lib" / "libflint.a").toString ]
-  }
 
   let name := nameToStaticLib "leanffi"
   let ffiO ← fetch <| pkg.target ``leanarb.o
-  let libFile := pkg.buildDir / "lib" / name
-  let linkJobs := #[ffiO]
-  let linkArgs := #[
-    "-L" ++ arbDir.toString,
-    "-I" ++ arbDir.toString,
-    "-larb",
-    "-L" ++ flintDir.toString,
-    "-I" ++ flintDir.toString,
-    "-lflint", 
-    "-lmpfr", 
-    "-lgmp"]
-  --buildLeanSharedLib (pkg.buildDir / "lib" / name) #[ffiO]
-  let name := libFile.fileName.getD libFile.toString
-  buildFileAfterDepArray libFile linkJobs
-    (extraDepTrace := computeHash linkArgs) fun links => do
-      compileSharedLib name libFile (links.map toString ++ linkArgs) (← getLeanc)
+  buildStaticLib (pkg.buildDir / "src" / name) #[ffiO]
 
 @[defaultTarget]
 lean_exe ffi {
