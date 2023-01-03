6d113d78652c1c521ea12dcd97c4d09198393cfa
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-01-03 14:10:05 +0100

    feat: no ArithVal, simplify and everything in ArithExpr

--- CvxLean/Lib/Approximation/ArithExpr.lean
+++ CvxLean/Lib/Approximation/ArithExpr.lean
@@ -3,8 +3,6 @@ import Mathlib.Tactic.NormNum
 import Mathlib.Tactic.NormCast
 import Std.Data.Nat.Gcd
 
-open Lean
-
 /- Typeclasses for square root, exponential and logarithm. -/
 
 class Srt (α : Type u) where
@@ -26,60 +24,58 @@ end Nat
 
 namespace Rat 
 
-axiom den_nonzero : ∀ (x : Rat), x.den ≠ 0
+-- axiom den_nonzero : ∀ (x : Rat), x.den ≠ 0
 
-instance : HPow Rat Nat Rat where 
-  hPow x n := mkRat (x.num ^ n) (x.den ^ n)
+-- instance : HPow Rat Nat Rat where 
+--   hPow x n := mkRat (x.num ^ n) (x.den ^ n)
 
 instance : Inv Rat where 
   inv := Rat.inv
 
-section Lemmas 
+-- section Lemmas 
 
-lemma neg_zero : (-0 : Rat) = 0 := rfl
+-- lemma neg_zero : (-0 : Rat) = 0 := rfl
 
-lemma neg_num {x : Rat} : (-x).num = -(x.num) := rfl
+-- lemma neg_neg (x : Rat) : -(-x) = x := by 
+--   show Rat.neg (Rat.neg x) = x
+--   simp [Rat.neg]
 
-lemma neg_neg (x : Rat) : -(-x) = x := by 
-  show Rat.neg (Rat.neg x) = x
-  simp [Rat.neg]
+-- lemma neg_neg_of_pos {x : Rat} : 0 < x → -x < 0 := 
+--   sorry
 
-lemma neg_neg_of_pos {x : Rat} : 0 < x → -x < 0 := 
-  sorry
+-- lemma lt_of_neg_lt_neg {x y : Rat} : -x < -y → y < x := 
+--   sorry
 
-lemma lt_of_neg_lt_neg {x y : Rat} : -x < -y → y < x := 
-  sorry
+-- lemma pos_iff_num_pos {x : Rat} : 0 < x ↔ 0 < x.num := 
+--   sorry
 
-lemma pos_iff_num_pos {x : Rat} : 0 < x ↔ 0 < x.num := 
-  sorry
+-- lemma den_pos_of_nz_den {x : Rat} (hden : x.den ≠ 0) : (0 : Int) < x.den := 
+--   sorry
 
-lemma den_pos_of_nz_den {x : Rat} (hden : x.den ≠ 0) : (0 : Int) < x.den := 
-  sorry
+-- lemma inv_pos_of_pos_nz_den {x : Rat} (hx : 0 < x) (hden : x.den ≠ 0) 
+--   : 0 < x⁻¹ := 
+--   sorry
 
-lemma inv_pos_of_pos_nz_den {x : Rat} (hx : 0 < x) (hden : x.den ≠ 0) 
-  : 0 < x⁻¹ := 
-  sorry
+-- -- TODO: move 
+-- lemma Int.ofNat_div (n m : Nat) 
+--   : Int.ofNat (n / m) = Int.ofNat n / Int.ofNat m := rfl
 
--- TODO: move 
-lemma Int.ofNat_div (n m : Nat) 
-  : Int.ofNat (n / m) = Int.ofNat n / Int.ofNat m := rfl
+-- -- TODO: move 
+-- lemma Int.div_pos_of_le {x y : Int} (hy : 0 < y) (hle : y ≤ x) : 0 < x / y := 
+--   sorry
 
--- TODO: move 
-lemma Int.div_pos_of_le {x y : Int} (hy : 0 < y) (hle : y ≤ x) : 0 < x / y := 
-  sorry
+-- lemma Nat.le_cast {x y : ℕ} (h : x ≤ y) 
+--   : (Nat.cast x : Int) ≤ Nat.cast y :=
+--   sorry
 
-lemma Nat.le_cast {x y : ℕ} (h : x ≤ y) 
-  : (Nat.cast x : Int) ≤ Nat.cast y :=
-  sorry
+-- lemma mul_pos_of_pos_pos {x y : Rat} (hx : 0 < x) (hy : 0 < y) : 0 < x * y :=
+--   sorry
 
-lemma mul_pos_of_pos_pos {x y : Rat} (hx : 0 < x) (hy : 0 < y) : 0 < x * y :=
-  sorry
+-- lemma div_pos_of_pos_pos {x y : Rat} 
+--   (hx : 0 < x) (hy : 0 < y) (hyden : y.den ≠ 0) : 0 < x / y := 
+--   sorry
 
-lemma div_pos_of_pos_pos {x y : Rat} 
-  (hx : 0 < x) (hy : 0 < y) (hyden : y.den ≠ 0) : 0 < x / y := 
-  sorry
-
-end Lemmas 
+-- end Lemmas 
 
 section Rounding
 
@@ -178,29 +174,29 @@ lemma mem_log {α} [Log α] [LE α] (x : α) (I : Interval? α)
 
 /- Generic arithmetic values. -/
 
-inductive ArithVal (α : Type u) : Type u
-  | scalar : α → ArithVal α
-  | vector (n) : (Fin n → α) → ArithVal α
-  | matrix (n m) : (Fin n → Fin m → α) → ArithVal α
+-- inductive ArithVal (α : Type u) : Type u
+--   | scalar : α → ArithVal α
+--   | vector (n) : (Fin n → α) → ArithVal α
+--   | matrix (n m) : (Fin n → Fin m → α) → ArithVal α
 
-namespace ArithVal
+-- namespace ArithVal
 
-def elementWise (f : α → α) : ArithVal α → ArithVal α
-  | scalar x => scalar (f x)
-  | vector n v => vector n (fun i => f (v i))
-  | matrix n m A => matrix n m (fun i j => f (A i j))
+-- def elementWise (f : α → α) : ArithVal α → ArithVal α
+--   | scalar x => scalar (f x)
+--   | vector n v => vector n (fun i => f (v i))
+--   | matrix n m A => matrix n m (fun i j => f (A i j))
 
-instance [Neg α] : Neg (ArithVal α) := ⟨elementWise Neg.neg⟩
+-- instance [Neg α] : Neg (ArithVal α) := ⟨elementWise Neg.neg⟩
 
-instance [Inv α] : Inv (ArithVal α) := ⟨elementWise Inv.inv⟩
+-- instance [Inv α] : Inv (ArithVal α) := ⟨elementWise Inv.inv⟩
 
-instance [Srt α] : Srt (ArithVal α) := ⟨elementWise Srt.srt⟩
+-- instance [Srt α] : Srt (ArithVal α) := ⟨elementWise Srt.srt⟩
 
-instance [Exp α] : Exp (ArithVal α) := ⟨elementWise Exp.exp⟩
+-- instance [Exp α] : Exp (ArithVal α) := ⟨elementWise Exp.exp⟩
 
-instance [Log α] : Log (ArithVal α) := ⟨elementWise Log.log⟩
+-- instance [Log α] : Log (ArithVal α) := ⟨elementWise Log.log⟩
 
-end ArithVal
+-- end ArithVal
 
 /- Reals. -/
 
@@ -229,10 +225,10 @@ class RealLike (R : Type u) extends
 
 namespace RealLike
 
-lemma pos_iff_pos_ofReal {R} [RealLike R] {x : Rat} 
-  : 0 < x ↔ (0 : R) < ofRat x := by
-  rw [←ofRat_zero]
-  exact ofRat_lt_ofRat_iff
+-- lemma pos_iff_pos_ofReal {R} [RealLike R] {x : Rat} 
+--   : 0 < x ↔ (0 : R) < ofRat x := by
+--   rw [←ofRat_zero]
+--   exact ofRat_lt_ofRat_iff
 
 end RealLike
 
@@ -244,9 +240,13 @@ class Bounded (R : Type u) [RealLike R] (f : R → R) where
     x ∈ (I.map RealLike.ofRat : Interval? R) → 
     f x ∈ ((bound prec I).map RealLike.ofRat : Interval? R)
 
+-- def arithBound (R : Type u) [RealLike R] (f : R → R) (prec : Nat) [Bounded R f] 
+--   : ArithVal (Interval? Rat) → ArithVal (Interval? Rat) :=
+--   ArithVal.elementWise (Bounded.bound f prec)
+
 def arithBound (R : Type u) [RealLike R] (f : R → R) (prec : Nat) [Bounded R f] 
-  : ArithVal (Interval? Rat) → ArithVal (Interval? Rat) :=
-  ArithVal.elementWise (Bounded.bound f prec)
+  : Interval? Rat → Interval? Rat :=
+  Bounded.bound f prec
 
 namespace Neg
 
@@ -280,90 +280,58 @@ end Inv
 
 namespace Srt
 
-def ratIter : Nat → Nat → Rat → Rat 
-  | Nat.zero,   _,    x => if x = 0 then 1 else x
-  | Nat.succ n, prec, x => 
-      let y := ratIter n prec x
-      Rat.roundUp prec ((y + x / y) / 2)
+def sqrtLowerBound : Rat → Rat := fun x => x / 2
 
-mutual 
-  def ratUp (prec : Nat) (x : Rat) : Rat := 
-    if 0 < x then ratIter prec prec x else 
-    if x < 0 then -ratDown prec (-x) else 0
+def sqrtUpperBound : Rat → Rat := fun x => x / 2
 
-  def ratDown (prec : Nat) (x : Rat) : Rat := 
-    if 0 < x then Rat.divDown prec x (ratIter prec prec x) else 
-    if x < 0 then -ratUp prec (-x) else 0 
-end
-termination_by' measure (fun x => 
-  match x with 
-  | PSum.inl x => if x < 0 then 1 else 0
-  | PSum.inr x => if x < 0 then 1 else 0) 
-decreasing_by 
-  have hneg : ¬(-x < 0) := by
-    { intros hc; rw [←Rat.neg_zero] at hc; 
-      exact (by assumption : ¬(0 < x)) (Rat.lt_of_neg_lt_neg hc) }
-  show (if -x < 0 then 1 else 0) < (if x < 0 then 1 else 0) <;> 
-  rw [if_pos (by assumption : x < 0), if_neg hneg] <;> 
-  norm_num 
+instance (R : Type u) [RealLike R] : Bounded R Srt.srt where 
+  bound _ 
+    | some I => some <| ⟨sqrtLowerBound I.a, sqrtUpperBound I.b⟩
+    | none => none
+  bound_correct prec x I := sorry
 
-open RealLike
-
-variable {R} [RealLike R]
-
-lemma srt_ub_of_pos_pos (x b : R) 
-  (hsrt : srt x < b) (hb : 0 < b) (hx : 0 < x) :
-  srt x < (b + x / b) / 2 :=
-  srt_bound hb hx hsrt 
-
-lemma ratIter_bound (x : Rat) (prec n : Nat) (hx : (0 : R) < ofRat x) :
-  srt (ofRat x) < (ofRat (ratIter n prec x) : R) := by 
-  induction n with 
-  | zero => 
-      by_cases x = 0
-      . simp [ratIter, h, ofRat_zero, srt_zero, ofRat_one, zero_lt_one]
-      . simp [ratIter, h, srt_lt_pos hx]
-  | succ n ih => 
-      have unrounded : 
-        (srt (ofRat x) : R) < 
-        ofRat (((ratIter n prec x) + x / (ratIter n prec x)) / 2) := by 
-        simp [ofRat_div, ofRat_add, ofRat_ofNat]
-        refine srt_ub_of_pos_pos (ofRat x) (ofRat (ratIter n prec x)) ih ?_ hx 
-        exact lt_of_le_of_lt (srt_nonneg (ofRat x)) ih
-      exact lt_of_lt_of_le unrounded (ofRat_le_ofRat_iff.1 (Rat.roundUp_ub prec _))
-
-lemma ratIter_pos (x : Rat) (prec n : Nat) (hx : (0 : R) < ofRat x) :
-  (0 : R) < ofRat (ratIter n prec x) := 
-  lt_of_le_of_lt (srt_nonneg (ofRat x)) (ratIter_bound x prec n hx)
-
--- lemma lb_sqrt_lower_bound:
---   assumes "0 ≤ real_of_float x"
---   shows "0 ≤ real_of_float (lb_sqrt prec x)"
-
-lemma ratDown_lb (x : Rat) (prec n : Nat) (hx : (0 : R) < ofRat x) : 
-  (0 : R) < ofRat (ratDown prec x) := by
-  by_cases 0 < x 
-  . simp [ratDown, h]
-    have hIter := ratIter_pos x prec prec hx
-    rw [←pos_iff_pos_ofReal] at hx hIter ⊢
-    
-    sorry 
-  . exfalso ; apply h ; rw [←ofRat_zero] at hx ; exact (ofRat_lt_ofRat_iff.2 hx)
-    
 end Srt
+
+namespace Exp
+
+def expLowerBound : Rat → Rat := fun x => 1
+
+def expUpperBound : Rat → Rat := fun x => 1
+
+instance (R : Type u) [RealLike R] : Bounded R Exp.exp where 
+  bound _ 
+    | some I => some <| ⟨expLowerBound I.a, expUpperBound I.b⟩
+    | none => none
+  bound_correct prec x I := sorry
+
+end Exp
+
+namespace Log
+
+def logLowerBound : Rat → Rat := fun x => 0
+
+def logUpperBound : Rat → Rat := fun x => 0
+
+instance (R : Type u) [RealLike R] : Bounded R Log.log where 
+  bound _ 
+    | some I => some <| ⟨logLowerBound I.a, logUpperBound I.b⟩
+    | none => none
+  bound_correct prec x I := sorry
+
+end Log
 
 /- Arithmetic expressions where numerical values can be scalars, vectors, or 
 matrices. -/
 
 inductive ArithExpr (R : Type u) 
-  | val : ArithVal R → ArithExpr R
+  | val : R → ArithExpr R --ArithVal R → ArithExpr R
   | var : Nat → ArithExpr R
   | neg : ArithExpr R → ArithExpr R
   | inv : ArithExpr R → ArithExpr R
   | srt : ArithExpr R → ArithExpr R
   | exp : ArithExpr R → ArithExpr R
   | log : ArithExpr R → ArithExpr R
-  | pow : ArithExpr R → Nat → ArithExpr R 
+  --| pow : ArithExpr R → Nat → ArithExpr R 
   | add : ArithExpr R → ArithExpr R → ArithExpr R
   | mul : ArithExpr R → ArithExpr R → ArithExpr R 
 
@@ -375,40 +343,79 @@ def sub {R} (e₁ : ArithExpr R) (e₂ : ArithExpr R) : ArithExpr R :=
 def div {R} (e₁ : ArithExpr R) (e₂ : ArithExpr R) : ArithExpr R := 
   mul e₁ (inv e₂)
 
-open ArithVal
+-- open ArithVal
+
+def Option.map2 {α β γ : Type u} : (α → β → γ) → Option α → Option β → Option γ
+  | f, some x, some y => some (f x y)
+  | _, _, _ => none
 
 def interpret (R) [RealLike R] {n : Nat}
-  : ArithExpr R → (∀ i : Fin n, ArithVal R) → ArithVal R
-  | val x, _ => x
-  | var i, Is => if h : i < n then Is ⟨i, h⟩ else scalar 0
-  | neg e, Is => Neg.neg (interpret R e Is)
-  | inv e, Is => Inv.inv (interpret R e Is)
-  | srt e, Is => Srt.srt (interpret R e Is)
-  | exp e, Is => Exp.exp (interpret R e Is)
-  | log e, Is => Log.log (interpret R e Is)
-  | add e₁ e₂, Is => 
-      match (interpret R e₁ Is), (interpret R e₂ Is) with 
-        | scalar x, scalar y => scalar (x + y)
-        | scalar x, vector n y => vector n (fun i => x + y i)
-        | vector n x, scalar y => vector n (fun i => x i + y)
-        | vector n x, vector m y => 
-            if h : n = m then 
-              vector n (fun i => x i + y (h ▸ i)) 
-            else scalar 0
-        | _, _ => scalar 0
-  | _, _ => sorry
+  -- : ArithExpr R → (∀ i : Fin n, ArithVal R) → ArithVal R
+  : ArithExpr Rat → (Fin n → R) → Option R
+  | val x, _ => some <| RealLike.ofRat x
+  | var i, vs => if h : i < n then vs ⟨i, h⟩ else none
+  | neg e, vs => Option.map Neg.neg (interpret R e vs)
+  | inv e, vs => Option.map Inv.inv (interpret R e vs)
+  | srt e, vs => Option.map Srt.srt (interpret R e vs)
+  | exp e, vs => Option.map Exp.exp (interpret R e vs)
+  | log e, vs => Option.map Log.log (interpret R e vs)
+  | add e₁ e₂, vs => Option.map2 Add.add (interpret R e₁ vs) (interpret R e₂ vs)
+      -- match (interpret R e₁ Is), (interpret R e₂ Is) with 
+      --   | scalar x, scalar y => scalar (x + y)
+      --   | scalar x, vector n y => vector n (fun i => x + y i)
+      --   | vector n x, scalar y => vector n (fun i => x i + y)
+      --   | vector n x, vector m y => 
+      --       if h : n = m then 
+      --         vector n (fun i => x i + y (h ▸ i)) 
+      --       else scalar 0
+      --   | _, _ => scalar 0
+  | mul e₁ e₂, vs => Option.map2 Mul.mul (interpret R e₁ vs) (interpret R e₂ vs)
+
+instance : Min Rat where 
+  min x y := if x < y then x else y
+
+instance : Max Rat where 
+  max x y := if x < y then y else x
 
 def approx (R) [RealLike R] {n : Nat} (prec : Nat) 
-  : ArithExpr Rat → (∀ i : Fin n, ArithVal (Interval? Rat)) → ArithVal (Interval? Rat)
-  | val (scalar x), _ => scalar <| some ⟨x, x⟩
-  | val (vector n x), _ => vector n <| fun i => some ⟨x i, x i⟩
-  | val (matrix n m x), _ => matrix n m <| fun i j => some ⟨x i j, x i j⟩
-  | var i, Is => if h : i < n then Is ⟨i, h⟩ else scalar none 
+  -- : ArithExpr Rat → (∀ i : Fin n, ArithVal (Interval? Rat)) → ArithVal (Interval? Rat)
+  -- | val (scalar x), _ => scalar <| some ⟨x, x⟩
+  -- | val (vector n x), _ => vector n <| fun i => some ⟨x i, x i⟩
+  -- | val (matrix n m x), _ => matrix n m <| fun i j => some ⟨x i j, x i j⟩
+  : ArithExpr Rat → (Fin n → Interval? Rat) → Interval? Rat
+  | val x, _ => some ⟨x, x⟩
+  | var i, Is => if h : i < n then Is ⟨i, h⟩ else none 
   | neg e, Is => arithBound R Neg.neg prec (approx R prec e Is)
   | inv e, Is => arithBound R Inv.inv prec (approx R prec e Is)
-  | srt e, Is => sorry
-  | exp e, Is => sorry
-  | log e, Is => sorry
-  | _, _ => sorry
+  | srt e, Is => arithBound R Srt.srt prec (approx R prec e Is)
+  | exp e, Is => arithBound R Exp.exp prec (approx R prec e Is)
+  | log e, Is => arithBound R Log.log prec (approx R prec e Is)
+  | add e₁ e₂, Is => 
+      match approx R prec e₁ Is, approx R prec e₂ Is with 
+        | some I₁, some I₂ => some <| ⟨I₁.a + I₂.a, I₁.b + I₂.b⟩
+        | _, _ => none 
+  | mul e₁ e₂, Is => 
+      match approx R prec e₁ Is, approx R prec e₂ Is with 
+        | some I₁, some I₂ => 
+            let ac := I₁.a * I₂.a
+            let ad := I₁.a * I₂.b
+            let bc := I₁.b * I₂.a
+            let bd := I₁.b * I₂.b
+            some <| ⟨min ac (min ad (min bc bd)), max ac (max ad (max bc bd))⟩
+        | _, _ => none
+
+def boundedVars {R} [RealLike R] {n : Nat} 
+  (vs : Fin n → R) (Is : Fin n → Interval? Rat) : Prop := 
+  ∀ i : Fin n, vs i ∈ Interval?.map (RealLike.ofRat (R := R)) (Is i)
+
+theorem approx_correct (R) [RealLike R] {n : Nat} 
+  (e : ArithExpr Rat) (vs : Fin n → R) (Is : Fin n → Interval? Rat)
+  (prec : Nat) (h : boundedVars vs Is) 
+  : if let some x := interpret R e vs then 
+      x ∈ Interval?.map (RealLike.ofRat (R := R)) (approx R prec e Is)
+    else True :=
+  sorry
+  --by induction e generalizing vs xs with
+  
 
 end ArithExpr
