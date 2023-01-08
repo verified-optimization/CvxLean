7896fb01ead4cc8f7ef4870943a05ee16d248ad4
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-01-08 18:51:55 +0100

    feat: axiom Ball.sqrt_correct

---
+++ ffi/lean/Axiom.lean
@@ -0,0 +1,8 @@
+import FFI
+import Mathbin.Data.Real.Sqrt
+
+noncomputable def Real.ofRat (x : Lean.Rat) : Real := 
+  if x.num < 0 then -1 else 1 * (x.num.natAbs : Real) / (x.den : Real)
+
+axiom Ball.sqrt_correct : ∀ (prec : Nat) (b : Ball Lean.Rat), 
+  b.map (Real.sqrt ∘ Real.ofRat) ⊆ (Ball.sqrt prec b).map Real.ofRat
