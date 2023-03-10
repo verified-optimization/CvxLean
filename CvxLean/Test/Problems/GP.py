8ec2154ea3b4c680eb0b00634d3a9dd7829ef53a
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-03-10 17:09:53 +0000

    wip: some GP experiments with CVXPY

--- CvxLean/Test/Problems/GP.py
+++ CvxLean/Test/Problems/GP.py
@@ -1,38 +1,39 @@
 import cvxpy as cp
 
-x = cp.Variable(pos=True, name="x")
-y = cp.Variable(pos=True, name="y")
-z = cp.Variable(pos=True, name="z")
+x3 = cp.Variable(pos=True)
+y3 = cp.Variable(pos=True)
+z3 = cp.Variable(pos=True)
 
-w = cp.Variable(pos=True, name="w")
+exp3 = cp.Problem(
+    cp.Maximize(x3 / y3), [
+    2 <= x3,
+    x3 <= 3,
+    x3 ** 2 + 3 * (y3 / z3) <= y3 ** 0.5,
+])
+exp3.solve(gp=True)
 
-objective_fn = x * y * z
-constraints = [
-    x * y * z + 2 * x * z <= 10, 
-    x <= 2 * y, 
-    y <= 2 * x, 
-    z >= 1]
+print(f"exp3.status   : {exp3.status}.") # optimal
+print(f"exp3.value    : {exp3.value}." ) # 4.641588833612779
+print(f"exp3.solution : {x3.value}."   ) # 2.154434690031884
 
-# 
+x4 =cp.Variable((1))
+y4 =cp.Variable((1))
+z4 =cp.Variable((1))
 
-# print("DGP check")
-# assert w.is_log_log_affine()
-# assert (cp.Constant(1)).is_log_log_affine()
-# assert (w + 1).is_log_log_convex()
-# assert (w + 1).is_log_log_concave()
-# assert (w + 1).is_log_log_affine()
-# assert (x + 1).is_dgp()
+# exp4 = cp.Problem(
+#     cp.Minimize(cp.exp(-2 * x4)), [
+#     #cp.log(cp.exp(2 * x4) + cp.exp(x4)) <= cp.log(11)
+#     cp.log(x4) >= 10
+# ])
 
-# assert objective_fn.is_log_log_concave()
-# assert all(constraint.is_dgp() for constraint in constraints)
+exp4 = cp.Problem(
+    cp.Maximize(x4 - y4), [
+    cp.log(2) <= x4, 
+    x4 <= cp.log(3),
+    cp.log_sum_exp(cp.hstack([2 * x4 + (-0.5) * y4, 3 * 0.5 * y4 + (-1) * z4])) <= 0,
+])
+exp4.solve()
 
-problem = cp.Problem(cp.Maximize(objective_fn), constraints)
-
-print(problem)
-print("Is this problem DGP?", problem.is_dgp())
-
-problem.solve(gp=True, verbose=True)
-
-print(f"problem.status   : {problem.status}.")
-print(f"problem.value    : {problem.value}." )
-print(f"problem.solution : {x.value}, {y.value}, {z.value}, {w.value}.")
+print(f"exp4.status   : {exp4.status}.") # optimal
+print(f"exp4.value    : {exp4.value}." ) # 4.641588833612779
+print(f"exp4.solution : {x4.value}."   ) # [0.5]
