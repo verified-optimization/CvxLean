561d4a0352b45dbd381527057f22ad52d0274c01
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-05-09 09:27:42 -0400

    refactor: remove Approximation

--- ffi/lean/Axiom.lean
+++
@@ -1,8 +0,0 @@
-import FFI
-import Mathbin.Data.Real.Sqrt
-
-noncomputable def Real.ofRat (x : Rat) : Real := 
-  if x.num < 0 then -1 else 1 * (x.num.natAbs : Real) / (x.den : Real)
-
-axiom Ball.sqrt_correct : ∀ (prec : Nat) (b : Ball Rat), 
-  b.map (Real.sqrt ∘ Real.ofRat) ⊆ (Ball.sqrt prec b).map Real.ofRat
