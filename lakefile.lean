7569d9d6b78a90af3e3c061303253d82a9319ed8
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-05-24 20:31:54 +0100

    feat: rewriting with egg

--- lakefile.lean
+++ lakefile.lean
@@ -11,3 +11,31 @@ lean_lib CvxLeanTest
 
 @[default_target]
 lean_lib CvxLean
+
+def compileCargo (name : String) (manifestFile : FilePath)
+ (cargo : FilePath := "cargo") : LogIO Unit := do
+  logInfo s!"Creating {name}"
+  proc {
+    cmd := cargo.toString
+    args := #["build", "--release", "--manifest-path", manifestFile.toString]
+  }
+
+def buildCargo (targetFile : FilePath) (manifestFile : FilePath) 
+(targetDest : FilePath) (oFileJobs : Array (BuildJob FilePath)) :
+SchedulerM (BuildJob FilePath) :=
+  let name := targetFile.fileName.getD targetFile.toString
+  buildFileAfterDepArray targetFile oFileJobs fun _ => do
+    compileCargo name manifestFile
+    createParentDirs targetDest
+    proc {
+      cmd := "cp"
+      args := #[targetFile.toString, targetDest.toString]
+    }
+
+@[default_target]
+target «egg-convexify» (pkg : Package) : FilePath := do
+  let buildDir := pkg.dir / "rewriter"
+  let binFile := buildDir / "target" / "release" / "egg-convexify"
+  let dest := buildDir / "utils" / "egg-convexify"
+  let manifestFile := buildDir / "Cargo.toml"
+  buildCargo binFile manifestFile dest #[]
