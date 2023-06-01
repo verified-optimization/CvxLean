66f51ed2b97b7fbf9e7441963eb43000731cb0e2
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-06-01 17:20:05 +0100

    test: add quasiconvex example

--- CvxLean/Test/Problems/GP.lean
+++ CvxLean/Test/Problems/GP.lean
@@ -165,4 +165,20 @@ reduction red3/dcp3 : gp3 := by
 
 end GPTutorialPaper
 
+section QuasiConvex
+
+def qp1 := 
+  optimization (x y : ℝ) 
+    minimize - (sqrt x) / y
+    subject to 
+      h1 : 0 < y
+      h2 : (exp x) ≤ y
+
+reduction redq1/dcpq1 : qp1 := by
+  unfold qp1
+  -- convexify
+  exact done
+
+end QuasiConvex
+
 end GP
