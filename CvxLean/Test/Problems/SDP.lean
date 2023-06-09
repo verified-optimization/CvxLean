48207d6af22f53a83c4df473e77bf6f7762cb384
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-06-09 13:20:18 +0100

    fix: SDP test passes

--- CvxLean/Test/Problems/SDP.lean
+++ CvxLean/Test/Problems/SDP.lean
@@ -6,7 +6,7 @@ section SDP
 open CvxLean Minimization Real
 
 @[optimization_param]
-noncomputable def A1 : Matrix (Finₓ 2) (Finₓ 2) ℝ :=
+noncomputable def A1 : Matrix (Fin 2) (Fin 2) ℝ :=
 fun i j => 
   (#[#[23.90853599,  0.40930502]
    , #[ 0.79090389, 21.30303590]][i.val]!)[j.val]!
@@ -15,13 +15,13 @@ fun i j =>
 noncomputable def b1 : ℝ := 8.0
 
 @[optimization_param]
-noncomputable def C1 : Matrix (Finₓ 2) (Finₓ 2) ℝ :=
+noncomputable def C1 : Matrix (Fin 2) (Fin 2) ℝ :=
 fun i j => 
   (#[#[0.31561605, 0.97905625]
    , #[0.84321261, 0.06878862]][i.val]!)[j.val]!
 
 noncomputable def sdp1 :=
-  optimization (X : Matrix (Finₓ 2) (Finₓ 2) ℝ)
+  optimization (X : Matrix (Fin 2) (Fin 2) ℝ)
     minimize (Matrix.trace (Matrix.mul C1 X))
     subject to 
       h₁ : Matrix.trace (Matrix.mul A1 X) <= b1
