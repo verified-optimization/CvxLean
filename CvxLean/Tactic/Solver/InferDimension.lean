5a70387ef09784bf0ecba2f02e08722a7b315c25
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-07-10 14:32:06 -0400

    feat: safe inferDimension

--- CvxLean/Tactic/Solver/InferDimension.lean
+++ CvxLean/Tactic/Solver/InferDimension.lean
@@ -7,19 +7,19 @@ namespace CvxLean
 
 open Lean Lean.Meta
 
-unsafe def inferDimension (ty : Expr) : MetaM (List (Nat × Nat)) :=
+partial def inferDimension (ty : Expr) : MetaM (List (Nat × Nat)) :=
   match ty.consumeMData with
   | Expr.const ``Real _ => 
       return [(1, 1)]
   | Expr.forallE _ (Expr.app (Expr.const ``Fin _) nExpr) e _ => 
     match e with 
     | Expr.const ``Real _  => do
-        let n : Nat ← evalExpr Nat (mkConst ``Nat) nExpr
-        return [(n, 1)]
+        let n ← evalNat nExpr |>.run
+        return [(n.get!, 1)]
     | Expr.forallE _ (Expr.app (Expr.const ``Fin _) mExpr) (Expr.const ``Real _) _ => do 
-        let n : Nat ← evalExpr Nat (mkConst ``Nat) nExpr
-        let m : Nat ← evalExpr Nat (mkConst ``Nat) mExpr
-        return [(n, m)]
+        let n ← evalNat nExpr |>.run
+        let m ← evalNat mExpr |>.run
+        return [(n.get!, m.get!)]
     | _ => throwError "Unsupported type: {ty}"
   | Expr.app (Expr.app (Expr.app M FinN) FinM) R => do 
     match (M, FinN, FinM, R) with 
@@ -27,9 +27,9 @@ unsafe def inferDimension (ty : Expr) : MetaM (List (Nat × Nat)) :=
       Expr.app (Expr.const ``Fin _) nExpr, 
       Expr.app (Expr.const ``Fin _) mExpr,
       Expr.const ``Real _) => 
-        let n : Nat ← evalExpr Nat (mkConst ``Nat) nExpr
-        let m : Nat ← evalExpr Nat (mkConst ``Nat) mExpr
-        return [(n, m)]
+        let n ← evalNat nExpr |>.run
+        let m ← evalNat mExpr |>.run
+        return [(n.get!, m.get!)]
     | _ => throwError "Unsupported type: {ty}"
   | Expr.app (Expr.app (Expr.const ``Prod _) tyl) tyr => do
     let l ← inferDimension tyl
