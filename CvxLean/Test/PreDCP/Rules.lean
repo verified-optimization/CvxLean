cfd5957eb32eb71d8cf86c04c16aa5bd43ba40e5
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-08-15 12:48:35 +0200

    feat: time_cmd

--- CvxLean/Test/PreDCP/Rules.lean
+++ CvxLean/Test/PreDCP/Rules.lean
@@ -1,5 +1,6 @@
 import CvxLean.Command.Solve
 import CvxLean.Tactic.PreDCP.Convexify
+import CvxLean.Tactic.Conv.ConvOpt
 
 noncomputable section Rules
 
@@ -11,19 +12,42 @@ def invExpObj :=
     subject to 
       h : 1 ≤ x
 
-reduction invExpObjRedAuto/invExpObjAuto : invExpObj := by
+syntax (name := time_cmd) "time_cmd " command : command
+
+@[command_elab «time_cmd»]
+def evalTimeCmd : Lean.Elab.Command.CommandElab := fun stx => match stx with
+| `(time_cmd $cmd) => do
+  let before ← BaseIO.toIO IO.monoMsNow
+  Lean.Elab.Command.elabCommand cmd
+  let after ← BaseIO.toIO IO.monoMsNow
+  let diff := after - before
+  IO.println s!"{diff} ms"
+| _ => Lean.Elab.throwUnsupportedSyntax
+
+time_cmd reduction invExpObjRedAuto/invExpObjAuto : invExpObj := by
   unfold invExpObj
   convexify
   exact done
 
--- TODO(RFM): Use this lemma instead of exp_neg.
-lemma Real.exp_neg2 : ∀ x : ℝ, exp (-x) = 1 / exp x := by
-  intro x
-  rw [Real.exp_neg, inv_eq_one_div]
+-- def rewrite_objective [Preorder R] {cs : D → Prop}
+-- {f : D → R}
+-- {g : D → R}
+-- (hfg : ∀ x, cs x → f x = g x)
+-- (sol : Solution
+--     { objFun := g
+--       constraints := cs })  :
+-- Solution {objFun := f, constraints := cs} :=
+-- simple_reduction _ _ sol id id
+--   (fun {x} hx => le_of_eq (hfg x hx).symm)
+--   (fun {x} hx => le_of_eq (hfg x hx))
+--   (fun {x} hx => hx)
+--   (fun {x} hx => hx)
 
-reduction invExpObjRedManual/invExpObjManual : invExpObj := by
+time_cmd reduction invExpObjRedManual/invExpObjManual : invExpObj := by
   unfold invExpObj
   map_objFun_log
+  --apply rewrite_objective (f := fun x => log (1 / exp x)) (g := fun x => (log (exp (-x)))) (hfg := fun x cs => by dsimp; rw [←Real.exp_neg2])
   simp only [←Real.exp_neg2]
+  simp only [Real.log_exp]
 
 end Rules 
