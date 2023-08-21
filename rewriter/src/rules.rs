1c2c492cefdb09517e800043602c89e615235eee
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-08-21 17:49:47 +0200

    chroe: allow unused for simple_example_rules

--- rewriter/src/rules.rs
+++ rewriter/src/rules.rs
@@ -142,7 +142,8 @@ pub fn rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
     
 ] }
 
-pub fn example_rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
+#[allow(unused)]
+pub fn simple_example_rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
     rw!("mul-comm"; "(mul ?a ?b)" => "(mul ?b ?a)"),
 
     rw!("le-mul"; "(le ?a (mul ?b ?c))" => "(le (div ?a ?c) ?b)" 
