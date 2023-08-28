7741cccf2c820b523ff691b16ebc02ea2e3f1b82
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-08-28 11:13:41 +0200

    test: problem with `exp` and `sqrt`

--- CvxLean/Test/Problems/SO.lean
+++ CvxLean/Test/Problems/SO.lean
@@ -22,4 +22,22 @@ solve so1
 #eval so1.value    -- 2.101003
 #eval so1.solution -- (-1.414214, -5.828427)
 
+
+-- Example from ??? paper.
+
+def so2 :=
+  optimization (x : ℝ)
+    minimize (x)
+    subject to 
+      hx : 0 ≤ x
+      h : exp (-x) ≤ sqrt x
+
+solve so2
+
+#print so2.reduced
+
+#eval so2.status   -- "PRIMAL_AND_DUAL_FEASIBLE"
+#eval so2.value    -- 0.426303
+#eval so2.solution -- 0.426303
+
 end SO
