5c208f3cdc498e1da3b605fd6db9bd80be2e1231
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-09-13 15:28:18 +0100

    feat: aesop working for simple examples

--- CvxLean/Tactic/PreDCP/Aesop.lean
+++ CvxLean/Tactic/PreDCP/Aesop.lean
@@ -4,6 +4,7 @@ import CvxLean.Lib.Missing.Real
 import CvxLean.Tactic.DCP.Dcp
 import CvxLean.Tactic.DCP.AtomLibrary
 import CvxLean.Tactic.PreDCP.Convexify
+import CvxLean.Tactic.PreDCP.AesopRuleSet
 
 open Lean Lean.Elab Lean.Meta Lean.Elab.Tactic Lean.Elab.Term Lean.Elab.Command
 open Aesop
@@ -42,12 +43,10 @@ def closeIfDCP : RuleTac := fun input =>
           finally restoreState state
     return ⟨#[]⟩
 
--- Ignore assumptions.
-attribute [-aesop] Aesop.BuiltinRules.applyHyps
-attribute [-aesop] Aesop.BuiltinRules.assumption
-
 -- Only close goal with custom rule.
-attribute [aesop safe tactic] closeIfDCP
+attribute [aesop safe (rule_sets [convexify_rule_set]) tactic] closeIfDCP
+
+lemma x : (1 + 1) = 1 ↔ 2 = 1:= by rfl
 
 /-- -/
 def applyTargetedRewrite (rwIdx numConstrs : Nat) 
@@ -59,39 +58,49 @@ def applyTargetedRewrite (rwIdx numConstrs : Nat)
       let tacStx ← tac 
       let (rwWrapper, _) ← rewriteWrapperLemma rwIdx numConstrs
       let gs ← input.goal.apply (mkConst (`Minimization ++ rwWrapper))
-      dbg_trace "got here"
+      
       -- There should be two goals at this point: hrw and cn'.
-      if gs.length != 2 then
+      if gs.length != 3 then
+        restoreState state
         return ⟨#[]⟩
       
       let gToRw := gs[0]!
-      let gExpectedExpr := gs[1]!
+      let gSol := gs[1]!
+      let gExpectedExpr := gs[2]!
 
       -- Sanity check to make sure we are rewriting in the right place.
       if (← gToRw.getTag) != `hrw then
-        dbg_trace "hrw tag not found"
+        dbg_trace "hrw tag not found {← gToRw.getTag}"
+        restoreState state
         return ⟨#[]⟩
       
-      let tacStx ← `(tactic| (intros; $tacStx)) 
+      dbg_trace "got here {rwName}"
+      let tacStx ← `(tactic| (intros; $tacStx <;> rfl)) 
       let gsAfterRw ← Meta.runTactic gToRw (evalTacticAt tacStx)
+      dbg_trace "applied {rwName}"
 
       -- If the rewrite is successful, there should be no goals and the expected
       -- expression should be instantiated.
       if gsAfterRw.length != 0 || !(← gExpectedExpr.isAssigned) then
+        
+        restoreState state
         return ⟨#[]⟩
-
+      
+      dbg_trace "succeeded {rwName}"
+      
+      gSol.setTag Name.anonymous
+      
       -- Add rule application.
       let postState ← saveState
       let fullTacStx ← `(tactic| (apply $(mkIdent rwWrapper); $tacStx))
       let scriptBuilder? := mkScriptBuilder? true <| .ofTactic 1 <| 
               withTransparencySyntax TransparencyMode.default <| fullTacStx
-      let rapp : RuleApplication := ⟨#[], postState, scriptBuilder?⟩ 
+
+      let rapp : RuleApplication := ⟨#[gSol], postState, scriptBuilder?⟩ 
       return ⟨#[rapp]⟩
     catch _ => 
-      return ⟨#[]⟩
-    finally 
       restoreState state
-
+      return ⟨#[]⟩
 
 /-- This command adds an Aesop rule for any combination of rewrites, taking all
 the rewrites from  -/
@@ -112,43 +121,52 @@ def evalAddConvexityAesopRules : CommandElab := fun stx => match stx with
           -- We use `applyTargetedRewrite` to generate new definitions.
           let name := Name.mkSimple <| 
             "convexity_rw__" ++ rwName ++ "__" ++ toString loc ++ "_" ++ toString numConstrs 
-          -- let type : Expr := mkConst `RuleTac
-          -- let value : Expr := mkAppN 
-          --   (mkConst `applyTargetedRewrite) 
-          --   #[mkNatLit loc, mkNatLit numConstrs, mkStrLit rwName]
-          -- let reducibility := Lean.ReducibilityHints.regular 0
-          -- let safety := DefinitionSafety.safe
-          -- Lean.addDecl <| 
-          --   Declaration.defnDecl 
-          --   (mkDefinitionValEx name [] type value reducibility safety [])
           let name' := mkIdent name
           dbg_trace name'
           let loc' := Lean.Syntax.mkNumLit (toString loc)
           let numConstrs' :=  Lean.Syntax.mkNumLit (toString numConstrs)
           let rwName' := Lean.Syntax.mkStrLit rwName
           elabCommand (← `(def $name' : RuleTac := applyTargetedRewrite $loc' $numConstrs' $rwName'))
-          elabCommand (← `(attribute [aesop unsafe tactic] $name'))    
+          elabCommand (← `(attribute [aesop unsafe (rule_sets [convexify_rule_set]) tactic] $name'))    
 | _ => throwUnsupportedSyntax
 
 add_convexity_aesop_rules
 
-#print convexity_rw__map_objFun_log__0_1
-
 section Test
+
+-- def p := 
+--   optimization (x : ℝ)
+--     minimize (x)
+--     subject to   
+--       h1 : 0 < x
+--       h2 : 1 / (sqrt x) ≤ (exp x)
 
 def p := 
   optimization (x : ℝ)
     minimize (x)
-    subject to   
-      h1 : 0 < x
-      h2 : 1 / (sqrt x) ≤ (exp x)
+    subject to
+      h2 : (exp x) * (exp x) ≤ exp x 
 
 set_option trace.Meta.debug true
 
-reduction red/q : p := by
-  aesop? (options := { maxRuleApplicationDepth := 100 })
+#check Aesop.SimpConfig
 
-end Test
+#check Real.exp_add
+
+set_option trace.aesop.ruleSet true
+
+set_option maxHeartbeats 1000000
+set_option maxRecDepth 1000000
+reduction red/q : p := by
+  aesop? 
+    (simp_options := { 
+      maxDischargeDepth := 0,
+      enabled := false })
+    (rule_sets [-builtin, -default, convexify_rule_set])
+    (options := { 
+      strategy := Strategy.breadthFirst,
+      maxRuleApplications := 10000,
+      maxRuleApplicationDepth := 100 })
 
 end Test 
 
