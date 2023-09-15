71a8103e00ebe2376da5303fb2a98d40dbe3e51e
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-09-15 16:20:49 +0100

    feat: affine multiplication by nonpositive constant

--- CvxLean/Tactic/DCP/AtomLibrary.lean
+++ CvxLean/Tactic/DCP/AtomLibrary.lean
@@ -167,6 +167,28 @@ optimality by
   intros y' hx
   apply mul_le_mul_of_nonneg_right hx hy
 
+declare_atom mul3 [affine] (x : ℝ)& (y : ℝ)- : x * y :=
+bconditions (hx : x ≤ 0)
+homogenity by
+  simp
+  ring
+additivity by
+  ring
+optimality by
+  intros y' hy
+  apply mul_le_mul_of_nonpos_left hy hx
+
+declare_atom mul4 [affine] (x : ℝ)- (y : ℝ)& : x * y :=
+bconditions (hy : y ≤ 0)
+homogenity by
+  change _ * _ + _ = (_ * _) * _ + _ * _
+  ring
+additivity by
+  ring
+optimality by
+  intros y' hx
+  apply mul_le_mul_of_nonpos_right hx hy
+
 end RealAffine
 
 -- Affine operations on vectors.
