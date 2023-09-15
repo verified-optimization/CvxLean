3a8d1d283e7881fac9d46843f5d01053f5ea5e67
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-09-15 22:24:32 +0100

    test: check DGP and DQCP in main example

--- CvxLean/Test/PreDCP/mainexample.py
+++ CvxLean/Test/PreDCP/mainexample.py
@@ -4,7 +4,20 @@ x = cp.Variable(pos=True)
 
 p = cp.Problem(
     cp.Minimize(x), [
-        -x <= cp.log(cp.sqrt(x))
+        1 / cp.sqrt(x) <= cp.exp(x)
     ])
+
+assert(not p.is_dgp())
+assert(not p.is_dqcp())
+assert(not p.is_dcp())
+
+p = cp.Problem(
+    cp.Minimize(x), [
+        cp.exp(-x) <= cp.sqrt(x)
+    ])
+
+assert(not p.is_dgp())
+assert(p.is_dqcp())
+assert(p.is_dcp())
 
 p.solve(verbose=True)
