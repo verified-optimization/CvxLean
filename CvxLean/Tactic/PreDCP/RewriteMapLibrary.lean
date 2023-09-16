f1a8531e4a34791e2f5354be6e2af90059fe56de
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-09-16 15:53:48 +0100

    feat: settled on a set of rewrite rules

--- CvxLean/Tactic/PreDCP/RewriteMapLibrary.lean
+++ CvxLean/Tactic/PreDCP/RewriteMapLibrary.lean
@@ -18,15 +18,19 @@ lemma Real.log_eq_log {x y : ℝ} (hx : 0 < x) (hy : 0 < y) : Real.log x = Real.
 lemma Real.div_pow_eq_mul_pow_neg {a b c : ℝ} (hb : 0 ≤ b) : a / (b ^ c) = a * b ^ (-c) := by
   rw [div_eq_mul_inv, ←rpow_neg hb]
 
--- TODO(RFM): Move.
-lemma Real.exp_neg_eq_one_div (x : ℝ) : exp (-x) = 1 / exp x := by
-  rw [exp_neg, inv_eq_one_div]
+-- TODO: Move.
+lemma Real.one_div_eq_pow_neg_one {a : ℝ} (ha : 0 < a) : 1 / a = a ^ (-1) := by 
+  rw [Real.rpow_neg (le_of_lt ha), rpow_one, div_eq_mul_inv, one_mul] 
 
 -- TODO: Move.
 lemma Real.pow_half_two {x : ℝ} (hx : 0 ≤ x) : (x ^ (1 / 2)) ^ 2 = x := by
   show Real.rpow (Real.rpow _ _) _ = _
   rw [rpow_eq_pow, rpow_eq_pow, ← rpow_mul hx]
   norm_num
+
+-- TODO(RFM): Move.
+lemma Real.exp_neg_eq_one_div (x : ℝ) : exp (-x) = 1 / exp x := by
+  rw [exp_neg, inv_eq_one_div]
 
 syntax "simp_or_rw" "[" Lean.Parser.Tactic.rwRule "]" : tactic
 
@@ -82,6 +86,9 @@ register_rewrite_map "log_le_log-rev" ; "(le ?a ?b)" => "(le (log ?a) (log ?b))"
   rewrite [←Real.log_le_log (by positivity) (by positivity)];
 
 register_rewrite_map "pow_two_le_pow_two-rev"; "(le ?a ?b)" => "(le (pow ?a 2) (pow ?b 2))" := 
+  rewrite [Real.pow_two_le_pow_two (by positivity) (by positivity)];
+
+register_rewrite_map "pow_two_le_pow_two-rev"; "(le ?a ?b)" => "(le (pow ?a 2) (pow ?b 2))" := 
   rewrite [←Real.pow_two_le_pow_two (by positivity) (by positivity)];
 
 
@@ -115,20 +122,32 @@ register_rewrite_map "mul_assoc" ; "(mul (mul ?a ?b) ?c)" => "(mul ?a (mul ?b ?c
 register_rewrite_map "add_sub" ; "(add ?a (sub ?b ?c))" => "(sub (add ?a ?b) ?c)" := 
   simp_or_rw [add_sub];
 
+register_rewrite_map "add_sub-rev" ; "(add ?a (sub ?b ?c))" => "(sub (add ?a ?b) ?c)" := 
+  simp_or_rw [←add_sub];
+
 register_rewrite_map "add_mul" ; "(mul (add ?a ?b) ?c)" => "(add (mul ?a ?c) (mul ?b ?c))" :=
   simp_or_rw [add_mul];
 
 register_rewrite_map "add_mul-rev" ; "(add (mul ?a ?c) (mul ?b ?c))" => "(mul (add ?a ?b) ?c)" :=
-  simp_or_rw [add_mul];
+  simp_or_rw [←add_mul];
 
 register_rewrite_map "mul_add" ; "(mul ?a (add ?b ?c))" => "(add (mul ?a ?b) (mul ?a ?c))" :=
   simp_or_rw [mul_add];
+
+register_rewrite_map "mul_add-rev" ; "(add (mul ?a ?b) (mul ?a ?c))" => "(mul ?a (add ?b ?c))" :=
+  simp_or_rw [←mul_add];
+
+register_rewrite_map "mul_sub" ; "(mul ?a (sub ?b ?c))" => "(sub (mul ?a ?b) (mul ?a ?c))" :=
+  simp_or_rw [←mul_sub];
 
 register_rewrite_map "mul_sub-rev" ; "(sub (mul ?a ?b) (mul ?a ?c))" => "(mul ?a (sub ?b ?c))" :=
   simp_or_rw [←mul_sub];
 
 register_rewrite_map "add_div" ; "(div (add ?a ?b) ?c)" => "(add (div ?a ?c) (div ?b ?c))" :=
   simp_or_rw [add_div];
+
+register_rewrite_map "add_div-rev" ; "(add (div ?a ?c) (div ?b ?c))" => "(div (add ?a ?b) ?c)" :=
+  simp_or_rw [←add_div];
 
 register_rewrite_map "mul_div" ; "(mul ?a (div ?b ?c))" => "(div (mul ?a ?b) ?c)"  :=
   simp_or_rw [mul_div];
@@ -142,8 +161,44 @@ register_rewrite_map "div_self" ; "(div ?a ?a)" => "1" :=
 
 /- Power and square root rules. -/
 
+register_rewrite_map "pow-add"; "(pow ?a (add ?b ?c))" => "(mul (pow ?a ?b) (pow ?a ?c))" :=
+  simp_or_rw [Real.rpow_add (by positivity)];
+
+register_rewrite_map "pow-add-rev"; "(mul (pow ?a ?b) (pow ?a ?c))" => "(pow ?a (add ?b ?c))" :=
+  simp_or_rw [←Real.rpow_add (by positivity)];
+
+register_rewrite_map "mul_pow"; "(mul (pow ?a ?n) (pow ?b ?n))" => "(pow (mul ?a ?b) ?n)" :=
+  simp_or_rw [Real.mul_rpow (by positivity) (by positivity)];
+
+register_rewrite_map "mul_pow-rev"; "(mul (pow ?a ?n) (pow ?b ?n))" => "(pow (mul ?a ?b) ?n)" :=
+  simp_or_rw [←Real.mul_rpow (by positivity) (by positivity)];
+
+register_rewrite_map "pow_mul"; "(pow ?a (mul ?n ?m))" => "(pow (pow ?a ?n) ?m)" :=
+  simp_or_rw [Real.rpow_mul (by positivity)];
+
+register_rewrite_map "pow_mul-rev"; "(pow (pow ?a ?n) ?m)" => "(pow ?a (mul ?n ?m))" :=
+  simp_or_rw [←Real.rpow_mul (by positivity)];
+
+register_rewrite_map "div_pow"; "(pow (div ?a ?b) ?n)" => "(div (pow ?a ?n) (pow ?b ?n))" :=
+  simp_or_rw [Real.div_rpow (by positivity) (by positivity)];
+
+register_rewrite_map "div_pow-rev"; "(div (pow ?a ?n) (pow ?b ?n))" => "(pow (div ?a ?b) ?n)" :=
+  simp_or_rw [←Real.div_rpow (by positivity) (by positivity)];
+
+register_rewrite_map "pow_sub"; "(pow ?a (sub ?b ?c))" => "(div (pow ?a ?b) (pow ?a ?c))" :=
+  simp_or_rw [Real.rpow_sub (by positivity)];
+
+register_rewrite_map "pow_sub-rev"; "(div (pow ?a ?b) (pow ?a ?c))" => "(pow ?a (sub ?b ?c))" :=
+  simp_or_rw [←Real.rpow_sub (by positivity)];
+
 register_rewrite_map "div_pow_eq_mul_pow_neg" ; "(div ?a (pow ?b ?c))" => "(mul ?a (pow ?b (neg ?c)))" :=
   simp_or_rw [Real.div_pow_eq_mul_pow_neg (by positivity)];
+
+register_rewrite_map "div_pow_eq_mul_pow_neg-rev" ; "(div ?a (pow ?b ?c))" => "(mul ?a (pow ?b (neg ?c)))" :=
+  simp_or_rw [←Real.div_pow_eq_mul_pow_neg (by positivity)];
+
+register_rewrite_map "one_div_eq_pow_neg_one"; "(div 1 ?a)" => "(pow ?a (neg 1))" :=
+  simp_or_rw [Real.one_div_eq_pow_neg_one (by positivity)];
 
 register_rewrite_map "sqrt_eq_rpow" ; "(sqrt ?a)" => "(pow ?a 0.5)" :=
   simp_or_rw [Real.sqrt_eq_rpow];
@@ -172,19 +227,28 @@ register_rewrite_map "exp_mul" ; "(exp (mul ?a ?b))" => "(pow (exp ?a) ?b)" :=
 register_rewrite_map "exp_mul-rev" ; "(pow (exp ?a) ?b)" => "(exp (mul ?a ?b))" :=
   simp_or_rw [←Real.exp_mul];
 
+register_rewrite_map "exp_neg_eq_one_div"; "(exp (neg ?a))" => "(div 1 (exp ?a))" :=
+  simp_or_rw [Real.exp_neg_eq_one_div];
+
 register_rewrite_map "exp_neg_eq_one_div-rev" ; "(div 1 (exp ?a))" => "(exp (neg ?a))" :=
   simp_or_rw [←Real.exp_neg_eq_one_div];
 
 register_rewrite_map "log_mul" ; "(log (mul ?a ?b))" => "(add (log ?a) (log ?b))" :=
   simp_or_rw [Real.log_mul (by positivity) (by positivity)];
 
+register_rewrite_map "log_mul-rev"; "(add (log ?a) (log ?b))" => "(log (mul ?a ?b))" :=
+  simp_or_rw [←Real.log_mul (by positivity) (by positivity)];
+
 register_rewrite_map "log_div" ; "(log (div ?a ?b))" => "(sub (log ?a) (log ?b))" :=
   simp_or_rw [Real.log_div (by positivity) (by positivity)];
 
-register_rewrite_map "log_exp" ; "(log (exp ?a))" => "?a" :=
-  simp_or_rw [Real.log_exp];
+register_rewrite_map "log_div-rev"; "(sub (log ?a) (log ?b))" => "(log (div ?a ?b))" :=
+  simp_or_rw [←Real.log_div (by positivity) (by positivity)];
 
 register_rewrite_map "exp_log" ; "(exp (log ?a))" => "?a" :=
   simp_or_rw [Real.exp_log (by positivity)];
+
+register_rewrite_map "log_exp" ; "(log (exp ?a))" => "?a" :=
+  simp_or_rw [Real.log_exp];
 
 end CvxLean
--- rewriter/src/rules.rs
+++ rewriter/src/rules.rs
@@ -11,31 +11,18 @@ pub fn rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
 
     /* Objective function rules. */
 
-    rw!("map_objFun_log"; "(prob (objFun ?a) ?cs)" => "(prob (objFun (log ?a)) ?cs)" 
+    rw!("map_objFun_log"; "(objFun ?a)" => "(objFun (log ?a))" 
         if is_gt_zero("?a")),
 
-    rw!("map_objFun_sq"; "(prob (objFun ?a) ?cs)" => "(prob (objFun (pow ?a 2)) ?cs)"
+    rw!("map_objFun_sq"; "(objFun ?a)" => "(objFun (pow ?a 2))"
         if is_ge_zero("?a")),
 
 
     /* Equality rules. */
+    // NOTE: many more rules could apply here, but in our examples, equalities 
+    // were either already affine or required applying logarithms to remove
+    // exponentials and make the affine.
 
-    // rw!("eq-add"; "(eq ?a (add ?b ?c))" => "(eq (sub ?a ?c) ?b)"),
-
-    // rw!("eq-sub"; "(eq ?a (sub ?b ?c))" => "(eq (add ?a ?c) ?b)"),
-
-    // rw!("eq-mul"; "(eq ?a (mul ?b ?c))" => "(eq (div ?a ?c) ?b)" 
-    //     if is_not_zero("?c")),
-
-    // rw!("eq-div"; "(eq ?a (div ?b ?c))" => "(eq (mul ?a ?c) ?b)" 
-    //     if is_not_zero("?c")),
-
-    // rw!("eq-sub-zero"; "(eq ?a ?b)" => "(eq (sub ?a ?b) 0)"
-    //     if is_not_zero("?b")),
-
-    // rw!("eq-div-one"; "(eq ?a ?b)" => "(eq (div ?a ?b) 1)" 
-    //     if is_not_zero("?b") if is_not_one("?b")),
-    
     rw!("log_eq_log"; "(eq ?a ?b)" => "(eq (log ?a) (log ?b))" 
         if is_gt_zero("?a") if is_gt_zero("?b")),
 
@@ -50,26 +37,11 @@ pub fn rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
 
     rw!("sub_le_iff_le_add-rev"; "(le ?a (add ?b ?c))" => "(le (sub ?a ?b) ?c)"),
 
-    // rw!("le-add"; "(le ?a (add ?b ?c))" => "(le (sub ?a ?c) ?b)"),
-
-    // rw!("le-mul"; "(le ?a (mul ?b ?c))" => "(le (div ?a ?c) ?b)" 
-    //     if is_not_zero("?c")),
-
     rw!("div_le_iff"; "(le (div ?a ?c) ?b)" => "(le ?a (mul ?b ?c))" 
         if is_gt_zero("?c")),
     
     rw!("div_le_iff-rev"; "(le ?a (mul ?b ?c))" => "(le (div ?a ?c) ?b)" 
         if is_gt_zero("?c")),
-
-    // rw!("le-div"; "(le ?a (div ?b ?c))" => "(le (mul ?a ?c) ?b)" 
-    //     if is_not_zero("?c")),
-
-    // rw!("le-sub-zero"; "(le ?a ?b)" => "(le (sub ?a ?b) 0)" 
-    //     if is_not_zero("?b")),
-
-    // NOTE(RFM): div-le-one? 
-    // rw!("div_le_one-rev"; "(le ?a ?b)" => "(le (div ?a ?b) 1)" 
-    //     if is_not_zero("?b") if is_not_one("?b")),
 
     rw!("log_le_log"; "(le (log ?a) (log ?b))" => "(le ?a ?b)" 
        if is_gt_zero("?a") if is_gt_zero("?b")),  
@@ -77,6 +49,9 @@ pub fn rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
     rw!("log_le_log-rev"; "(le ?a ?b)" => "(le (log ?a) (log ?b))"
         if is_gt_zero("?a") if is_gt_zero("?b")),
     
+    rw!("pow_two_le_pow_two"; "(le ?a ?b)" => "(le (pow ?a 2) (pow ?b 2))" 
+        if is_ge_zero("?a") if is_ge_zero("?b")),
+
     rw!("pow_two_le_pow_two-rev"; "(le ?a ?b)" => "(le (pow ?a 2) (pow ?b 2))" 
         if is_ge_zero("?a") if is_ge_zero("?b")),
 
@@ -103,30 +78,23 @@ pub fn rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
 
     rw!("add_sub"; "(add ?a (sub ?b ?c))" => "(sub (add ?a ?b) ?c)"),
 
+    rw!("add_sub-rev"; "(sub (add ?a ?b) ?c)" => "(add ?a (sub ?b ?c))"),
+
     rw!("add_mul"; "(mul (add ?a ?b) ?c)" => "(add (mul ?a ?c) (mul ?b ?c))"),
 
     rw!("add_mul-rev"; "(add (mul ?a ?c) (mul ?b ?c))" => "(mul (add ?a ?b) ?c)"),
 
     rw!("mul_add"; "(mul ?a (add ?b ?c))" => "(add (mul ?a ?b) (mul ?a ?c))"),
+
+    rw!("mul_add-rev"; "(add (mul ?a ?b) (mul ?a ?c))" => "(mul ?a (add ?b ?c))"),
     
-    // rw!("mul-sub"; "(mul ?a (sub ?b ?c))" => "(sub (mul ?a ?b) (mul ?a ?c))"),
+    rw!("mul_sub"; "(mul ?a (sub ?b ?c))" => "(sub (mul ?a ?b) (mul ?a ?c))"),
 
-    rw!("mul_sub-rev"; "(sub (mul ?a ?b) (mul ?a ?c))" => 
-        "(mul ?a (sub ?b ?c))"),
-
-    // rw!("sub-mul-right"; "(sub (mul ?a ?b) (mul ?c ?b))" => 
-    //     "(mul (sub ?a ?c) ?b)"),
-
-    // rw!("sub-mul-same-right"; "(sub ?a (mul ?b ?a))" => "(mul ?a (sub 1 ?b))"),
-
-    // rw!("sub-mul-same-left"; "(sub (mul ?a ?b) ?a)" => "(mul ?a (sub ?b 1))"),
+    rw!("mul_sub-rev"; "(sub (mul ?a ?b) (mul ?a ?c))" => "(mul ?a (sub ?b ?c))"),
 
     rw!("add_div"; "(div (add ?a ?b) ?c)" => "(add (div ?a ?c) (div ?b ?c))"),
 
-    // rw!("add-div"; "(add (div ?a ?b) (div ?c ?b))" => "(div (add ?a ?c) ?b)"),
-
-    // rw!("div-sub"; "(div (sub ?a ?b) ?c)" => "(sub (div ?a ?c) (div ?b ?c))" 
-    //     if is_not_zero("?c")),
+    rw!("add_div-rev"; "(add (div ?a ?c) (div ?b ?c))" => "(div (add ?a ?b) ?c)"),
 
     rw!("mul_div"; "(mul ?a (div ?b ?c))" => "(div (mul ?a ?b) ?c)"),
 
@@ -137,32 +105,44 @@ pub fn rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
 
     /* Power and square root rules. */
 
-    rw!("one_div_eq_pow_neg_one"; "(div 1 ?a)" => "(pow ?a (neg 1))" 
+    rw!("pow-add"; "(pow ?a (add ?b ?c))" => "(mul (pow ?a ?b) (pow ?a ?c))"
         if is_gt_zero("?a")),
 
-    // rw!("pow-add"; "(pow ?a (add ?b ?c))" => "(mul (pow ?a ?b) (pow ?a ?c))"),
+    rw!("pow-add-rev"; "(mul (pow ?a ?b) (pow ?a ?c))" => "(pow ?a (add ?b ?c))"
+        if is_gt_zero("?a")),
 
-    rw!("mul_pow"; "(mul (pow ?a ?n) (pow ?b ?n))" => "(pow (mul ?a ?b) ?n)"),
+    rw!("mul_pow"; "(pow (mul ?a ?b) ?n)" => "(mul (pow ?a ?n) (pow ?b ?n))"
+        if is_ge_zero("?a") if is_ge_zero("?b")),
 
-    rw!("mul_pow-rev"; "(pow (mul ?a ?b) ?n)" => "(mul (pow ?a ?n) (pow ?b ?n))"),
+    rw!("mul_pow-rev"; "(mul (pow ?a ?n) (pow ?b ?n))" => "(pow (mul ?a ?b) ?n)"
+        if is_ge_zero("?a") if is_ge_zero("?b")),
 
-    rw!("pow_mul"; "(pow ?a (mul ?n ?m))" => "(pow (pow ?a ?n) ?m)"),
+    rw!("pow_mul"; "(pow ?a (mul ?n ?m))" => "(pow (pow ?a ?n) ?m)" 
+        if is_ge_zero("?a")),
 
-    rw!("pow_mul-rev"; "(pow (pow ?a ?n) ?m)" => "(pow ?a (mul ?n ?m))"),
+    rw!("pow_mul-rev"; "(pow (pow ?a ?n) ?m)" => "(pow ?a (mul ?n ?m))"
+        if is_ge_zero("?a")),
 
-    rw!("div_pow"; "(div (pow ?a ?n) (pow ?b ?n))" => "(pow (div ?a ?b) ?n)"),
+    rw!("div_pow"; "(pow (div ?a ?b) ?n)" => "(div (pow ?a ?n) (pow ?b ?n))"
+        if is_ge_zero("?a") if is_ge_zero("?b")),
+    
+    rw!("div_pow-rev"; "(div (pow ?a ?n) (pow ?b ?n))" => "(pow (div ?a ?b) ?n)"
+        if is_ge_zero("?a") if is_ge_zero("?b")),
 
-    rw!("div_pow-rev"; "(pow (div ?a ?b) ?n)" => "(div (pow ?a ?n) (pow ?b ?n))"),
+    rw!("pow_sub"; "(pow ?a (sub ?b ?c))" => "(div (pow ?a ?b) (pow ?a ?c))" 
+        if is_gt_zero("?a")),
 
-    // rw!("pow-sub"; "(pow ?a (sub ?b ?c))" => "(div (pow ?a ?b) (pow ?a ?c))" 
-    //     if is_not_zero("?a")),
+    rw!("pow_sub-rev"; "(div (pow ?a ?b) (pow ?a ?c))" => "(pow ?a (sub ?b ?c))"
+        if is_gt_zero("?a")),
 
     rw!("div_pow_eq_mul_pow_neg"; "(div ?a (pow ?b ?c))" => "(mul ?a (pow ?b (neg ?c)))"
         if is_gt_zero("?b")),
 
-    // rw!("div-pow-same-right"; "(div ?a (pow ?a ?b))" => "(pow ?a (sub 1 ?b))"),
-
-    // rw!("div-pow-same-left"; "(div (pow ?a ?b) ?a)" => "(pow ?a (sub ?b 1))"),
+    rw!("div_pow_eq_mul_pow_neg-rev"; "(mul ?a (pow ?b (neg ?c)))" => "(div ?a (pow ?b ?c))"
+        if is_gt_zero("?b")),
+    
+    rw!("one_div_eq_pow_neg_one"; "(div 1 ?a)" => "(pow ?a (neg 1))" 
+        if is_gt_zero("?a")),
 
     rw!("sqrt_eq_rpow"; "(sqrt ?a)" => "(pow ?a 0.5)"),
 
@@ -185,17 +165,24 @@ pub fn rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
 
     rw!("exp_mul-rev"; "(pow (exp ?a) ?b)" => "(exp (mul ?a ?b))"),
 
-    // NOTE: exp_neg_eq_one_div_div?
+    rw!("exp_neg_eq_one_div"; "(exp (neg ?a))" => "(div 1 (exp ?a))"),
+
     rw!("exp_neg_eq_one_div-rev"; "(div 1 (exp ?a))" => "(exp (neg ?a))"),
 
-    rw!("exp_log"; "(exp (log ?a))" => "?a" if is_gt_zero("?a")),
-
     rw!("log_mul"; "(log (mul ?a ?b))" => "(add (log ?a) (log ?b))" 
+        if is_gt_zero("?a") if is_gt_zero("?b")),
+    
+    rw!("log_mul-rev"; "(add (log ?a) (log ?b))" => "(log (mul ?a ?b))" 
         if is_gt_zero("?a") if is_gt_zero("?b")),
 
     rw!("log_div"; "(log (div ?a ?b))" => "(sub (log ?a) (log ?b))" 
         if is_gt_zero("?a") if is_gt_zero("?b")),
+    
+    rw!("log_div-rev"; "(sub (log ?a) (log ?b))" => "(log (div ?a ?b))"
+        if is_gt_zero("?a") if is_gt_zero("?b")),
 
+    rw!("exp_log"; "(exp (log ?a))" => "?a" if is_gt_zero("?a")),
+    
     rw!("log_exp"; "(log (exp ?a))" => "?a"),  
 ] }
 
@@ -209,5 +196,5 @@ pub fn simple_example_rules() -> Vec<Rewrite<Optimization, Meta>> { vec![
     rw!("le-mul-rev"; "(le (div ?a ?c) ?b)" => "(le ?a (mul ?b ?c))" 
         if is_gt_zero("?c")),
     
-    rw!("inv-exp"; "(div 1 (exp ?a))" => "(exp (neg ?a))"),
+    rw!("exp_neg_eq_one_div-rev"; "(div 1 (exp ?a))" => "(exp (neg ?a))"),
 ] }
