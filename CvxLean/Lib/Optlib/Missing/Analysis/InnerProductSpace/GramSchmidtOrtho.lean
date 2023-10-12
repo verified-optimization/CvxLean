974600f9285e30900c9b0e14c7ef2a0099d19e9c
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-10-12 15:27:57 +0100

    fix: replace outdated mathlib lemmas

--- CvxLean/Lib/Optlib/Missing/Analysis/InnerProductSpace/GramSchmidtOrtho.lean
+++ CvxLean/Lib/Optlib/Missing/Analysis/InnerProductSpace/GramSchmidtOrtho.lean
@@ -12,7 +12,7 @@ local notation "⟪" x "," y "⟫" => @inner 𝕜 _ _ x y
 lemma repr_gramSchmidt_diagonal {i : ι} (b : Basis ι 𝕜 E) :
   b.repr (gramSchmidt 𝕜 b i) i = 1 := by 
   rw [gramSchmidt_def, LinearEquiv.map_sub, Finsupp.sub_apply, Basis.repr_self,
-    Finsupp.single_eq_same, sub_eq_self, LinearEquiv.map_sum, Finsupp.coe_finset_sum,
+    Finsupp.single_eq_same, sub_eq_self, map_sum, Finsupp.coe_finset_sum,
     Finset.sum_apply, Finset.sum_eq_zero]
   intros j hj
   rw [Finset.mem_Iio] at hj
