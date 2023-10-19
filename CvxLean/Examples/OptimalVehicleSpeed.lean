651c406ba2fd1bcc5b3733935c4e02c57d62ecc5
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-10-19 15:19:25 +0100

    wip: some simplifications in vehicle speed case study

--- CvxLean/Examples/OptimalVehicleSpeed.lean
+++ CvxLean/Examples/OptimalVehicleSpeed.lean
@@ -1,5 +1,6 @@
 import CvxLean.Command.Equivalence
 import CvxLean.Command.Solve
+import CvxLean.Tactic.Basic.Rename
 import CvxLean.Tactic.PreDCP.ChangeOfVariables
 
 noncomputable section OptimalVehicleSpeed
@@ -30,36 +31,38 @@ variable (Φ_pos : ∀ s, 0 < Φ s)
 variable (Φ_increasing : ∀ s1 s2, s1 ≤ s2 → Φ s1 ≤ Φ s2)
 variable (Φ_convex : ConvexOn ℝ ⊤ Φ)
 
-def optimalVehicleSpeed := 
+open FinsetInterval
+
+instance [i : Fact (0 < n)] : OfNat (Fin n) 0 := ⟨⟨0, i.out⟩⟩
+
+def optimalVehicleSpeed (_ : Fact (0 < n)) := 
   optimization (s : Fin n → ℝ)
     minimize ∑ i, (d i / s i) * Φ (s i) 
     subject to 
       hsmin : ∀ i, smin i ≤ s i 
       hsmax : ∀ i, s i ≤ smax i
-      hτmin : ∀ i, τmin i ≤ ∑ j in Finset.Iic i, d j / s j
-      hτmax : ∀ i, ∑ j in Finset.Iic i, d j / s j ≤ τmax i
+      hτmin : ∀ i, τmin i ≤ ∑ j in [[0, i]], d j / s j
+      hτmax : ∀ i, ∑ j in [[0, i]], d j / s j ≤ τmax i
 
-lemma positivity_test 
-  (x y : Fin n → ℝ) 
-  (hx : ∀ i, 0 < x i) 
-  (hxy : ∀ i, x i < y i)
-  (i : Fin n) : y i ≠ 0 := by {
-  have hx_i := hx i
-  have hxy_i := hxy i
-  linarith
-} 
-
--- TODO: Fatal error using conv inside equivalence.
+private lemma simp_fraction : ∀ s : Fin n → ℝ, ∀ i, d i / (d i / s i) = s i := by {
+  intros s i
+  have h : d i ≠ 0 := by {
+    have d_pos_i := d_pos i
+    linarith 
+  }
+  rw [← div_mul, div_self h, one_mul]
+}
 
 equivalence equiv/optimalVehicleSpeedConvex 
   {n : ℕ} 
+  (n_pos : 0 < n)
   (d : Fin n → ℝ)
   (d_pos : ∀ i, 0 < d i)
   (τmin τmax : Fin n → ℝ)
   (smin smax : Fin n → ℝ)
   (smin_pos : ∀ i, 0 < smin i)
   (Φ : ℝ → ℝ) : 
-  optimalVehicleSpeed d τmin τmax smin smax Φ := by 
+  optimalVehicleSpeed d τmin τmax smin smax Φ ⟨n_pos⟩ := by 
   unfold optimalVehicleSpeed
   apply Minimization.Equivalence.trans
   apply ChangeOfVariables.toEquivalence (fun t => d / t)
@@ -75,13 +78,37 @@ equivalence equiv/optimalVehicleSpeedConvex
       have d_pos_i := d_pos i
       linarith
       } }
+  -- IDEA: This can be done by change of variables by detecting that the 
+  -- variable is a vector.
   apply Minimization.Equivalence.trans
   apply MinimizationQ.rewrite_objective
-  { rintro s ⟨hsmin, _hsmax, _hτmin, _hτmax⟩
+  { rintro s ⟨_hsmin, _hsmax, _hτmin, _hτmax⟩
+    simp only [Pi.div_apply, simp_fraction d d_pos s]
+    rfl 
+  }
+  apply Minimization.Equivalence.trans
+  apply MinimizationQ.rewrite_constraint_3 
+  { rintro s _hsmin _hsmax _hτmax
+    simp only [Pi.div_apply, simp_fraction d d_pos s]
+    rfl
+  }
+  apply Minimization.Equivalence.trans
+  apply MinimizationQ.rewrite_constraint_4_last
+  { rintro s _hsmin _hsmax _hτmax
     simp only [Pi.div_apply]
-    have : ∀ x, d x / (d x / s x) = s x := sorry
-    simp only [this]
-    rfl }
+    simp only [simp_fraction d d_pos s]
+    rfl
+  }
   equivalence_rfl
+
+#print optimalVehicleSpeedConvex
+
+-- TODO: Fatal error using conv inside equivalence.
+
+-- TODO: rename_vars in equivalence mode.
+
+-- IDEA: simplify_objective [...], simplify_constraint i [...],
+-- Also rewrite_objective and rewrite_constraint. Potentially get rid of the 
+-- rewrite_constraint_x lemmas and handle that in meta.
 
 end OptimalVehicleSpeed
