2bae6d5437eb7f6b720b3d60563af67800102e33
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-11-08 16:06:06 +0000

    fix: no build errors with new Lean version

--- CvxLean/Meta/Missing/SubExpr.lean
+++ CvxLean/Meta/Missing/SubExpr.lean
@@ -11,12 +11,6 @@ variable [MonadReaderOf SubExpr m] [MonadWithReaderOf SubExpr m]
 variable [MonadLiftT MetaM m] [MonadControlT MetaM m]
 variable [MonadLiftT IO m]
 
-/-- Like `withBindingBody`, but gives us access to the FVar representing the bound variable. -/
-def withBindingBody' (n : Name) (x : Expr → m α) : m α := do
-  let e ← getExpr
-  Meta.withLocalDecl n e.binderInfo e.bindingDomain! fun fvar =>
-    descend (e.bindingBody!.instantiate1 fvar) 1 (x fvar)
-
 /-- Pretend that we actually want to delaborate the given expression. -/
 def withExpr (e : Expr) (x : m α) : m α := do
   withTheReader SubExpr (fun cfg => { cfg with expr := e, pos := cfg.pos }) x
