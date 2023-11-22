1060d0b911c2c1301f9038d2a2a1faa62b5abbc5
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-11-22 12:43:17 +0000

    fix: make equivalence handle meta-variables like reduction

--- CvxLean/Command/Equivalence.lean
+++ CvxLean/Command/Equivalence.lean
@@ -11,45 +11,63 @@ open Minimization
 partial def runEquivalenceTactic (mvarId : MVarId) (tacticCode : Syntax) :
   TermElabM Unit := do
   let code := tacticCode[1]
+  instantiateMVarDeclMVars mvarId
 
   withInfoHole mvarId do
-    let _remainingGoals ← Tactic.run mvarId do
+    let remainingGoals ← Tactic.run mvarId do
       withTacticInfoContext tacticCode do
         evalTactic code
 
+    match remainingGoals with
+    | [] => pure ()
+    | _ => reportUnsolvedGoals remainingGoals
+
+    synthesizeSyntheticMVars (mayPostpone := false)
+
 /-- Run equivalence tactic and return both the right-hand term (`q`) and the
 equivalence proof, of type `Equivalence p q`. -/
-def elabEquivalenceProof (prob : Expr) (stx : Syntax) : TermElabM (Expr × Expr) := do
+def elabEquivalenceProof (prob : Expr) (stx : Syntax) :
+  TermElabM (Expr × Expr) := do
   withRef stx do
-    let probTy ← inferType prob
-    let R := probTy.getArg! 1
-    let D := probTy.getArg! 0
-    let E ← Meta.mkFreshTypeMVar
-    let RPreorder ← Meta.mkFreshExprMVar
-      (some <| mkAppN (Lean.mkConst ``Preorder [levelZero]) #[R])
-    let prob₂ ← Meta.mkFreshExprMVar
-      (some <| mkApp2 (Lean.mkConst ``Minimization) E R)
-    let eqvTy := mkAppN
-      (mkConst ``Minimization.Equivalence)
-      #[R, D, E, RPreorder, prob, prob₂]
-    let eqv ← elabTerm stx (some eqvTy)
+    let syntheticMVarsSaved := (← get).syntheticMVars
+    modify fun s => { s with syntheticMVars := {} }
+    try
+      let probTy ← inferType prob
+      let R := probTy.getArg! 1
+      let D := probTy.getArg! 0
+      let E ← Meta.mkFreshTypeMVar
+      let RPreorder ← Meta.mkFreshExprMVar
+        (some <| mkAppN (Lean.mkConst ``Preorder [levelZero]) #[R])
+      let prob₂ ← Meta.mkFreshExprMVar
+        (some <| mkApp2 (Lean.mkConst ``Minimization) E R)
+      let eqvTy := mkAppN
+        (mkConst ``Minimization.Equivalence)
+        #[R, D, E, RPreorder, prob, prob₂]
+      let eqv ← elabTerm stx (some eqvTy)
 
-    let some mvarDecl ← getSyntheticMVarDecl? eqv.mvarId! | throwError "SyntheticMVarDecl not found."
+      let some mvarDecl ← getSyntheticMVarDecl? eqv.mvarId! |
+        throwError "SyntheticMVarDecl not found."
 
-    match mvarDecl.kind with
-    | SyntheticMVarKind.tactic tacticCode savedContext =>
-      withSavedContext savedContext do
-        runEquivalenceTactic eqv.mvarId! tacticCode
-    | _ => throwError "Expected SyntheticMVarDecl of kind tactic, got {mvarDecl.kind}"
+      modify fun s => { s with syntheticMVars := {} }
 
-    let eqvWitness ← instantiateMVars eqv
-    let eqvWitnessTy ← inferType eqvWitness
-    if eqvWitnessTy.isAppOf ``Minimization.Equivalence then
-      -- NOTE(RFM): Equivalence 0:R 1:D 2:E 3:RPreorder 4:p 5:q
-      let rhs := eqvWitnessTy.getArg! 5
-      return (rhs, ← instantiateMVars eqv)
-    else
-      throwError "Expected equivalence witness, got {eqvWitness}."
+      match mvarDecl.kind with
+      | SyntheticMVarKind.tactic tacticCode savedContext =>
+          withSavedContext savedContext do
+            runEquivalenceTactic eqv.mvarId! tacticCode
+      | _ => throwError "Expected SyntheticMVarDecl of kind tactic, got {mvarDecl.kind}"
+
+      let eqvWitness ← instantiateMVars eqv
+      let eqvWitnessTy ← inferType eqvWitness
+      if eqvWitnessTy.isAppOf ``Minimization.Equivalence then
+        -- NOTE(RFM): Equivalence 0:R 1:D 2:E 3:RPreorder 4:p 5:q
+        let rhs := eqvWitnessTy.getArg! 5
+        return (rhs, eqvWitness)
+      else
+        throwError "Expected equivalence witness, got {eqvWitness}."
+    finally
+      modify fun s => { s with syntheticMVars :=
+        s.syntheticMVars.mergeBy (fun _ _ a => a) syntheticMVarsSaved }
+
 
 syntax (name := equivalence)
   "equivalence" ident "/" ident declSig ":=" term : command
