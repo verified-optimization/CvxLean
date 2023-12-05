760f7eeb6b257e33caf85fe6349ba70ae2aa9e14
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-12-05 16:43:19 -0500

    feat: graph implementation for `quadOverLin`

--- CvxLean/Tactic/DCP/AtomLibrary/Fns/QuadOverLin.lean
+++ CvxLean/Tactic/DCP/AtomLibrary/Fns/QuadOverLin.lean
@@ -1,55 +1,57 @@
 import CvxLean.Tactic.DCP.Atoms
 import CvxLean.Tactic.DCP.AtomLibrary.Sets.Cones
+import CvxLean.Tactic.DCP.AtomLibrary.Sets.Le
 import CvxLean.Tactic.DCP.AtomLibrary.Fns.VecCons
 import CvxLean.Tactic.DCP.AtomLibrary.Fns.Add
 import CvxLean.Tactic.DCP.AtomLibrary.Fns.Sub
 import CvxLean.Tactic.DCP.AtomLibrary.Fns.Mul
+import CvxLean.Tactic.DCP.AtomLibrary.Fns.Exp
 import CvxLean.Lib.Math.Data.Vec
 
 namespace CvxLean
 
 open Real
 
-#check sqrt_le_iff
-
--- TODO: generalize to x : Fin n → ℝ
--- TODO: distinguish between nonincreasing and nondecreasing.
+-- TODO(RFM): generalize to x : Fin n → ℝ
+-- TODO(RFM): distinguish between nonincreasing and nondecreasing.
 declare_atom quadOverLin [convex] (x : ℝ)? (y : ℝ)- : x ^ 2 / y :=
 vconditions
   (hy : 0 < y)
-implementationVars (t : ℝ)
+implementationVars (t : ℝ) (y' : ℝ)
 implementationObjective (t)
 implementationConstraints
-  (c : soCone (y + t) ![y - t, 2 * x])
-solution (t := x ^ 2 / y)
+  (c1 : soCone (y + t) ![y - t, 2 * x])
+  (c2 : 0 ≤ t)
+  -- NOTE(RFM): This is a trick to make y strictly positive.
+  (c3 : exp y' ≤ y)
+solution (t := x ^ 2 / y) (y' := log y)
 solutionEqualsAtom by rfl
 feasibility
-  (c : by
+  (c1 : by
     have hynn := le_of_lt hy
     have hx2ynn : 0 ≤ x ^ 2 / y := by
-      rw [rpow_two]
-      exact div_nonneg (pow_two_nonneg x) hynn
+      rw [rpow_two]; exact div_nonneg (pow_two_nonneg x) hynn
     rw [soCone_add_sub_two_mul_of_nonneg _ hynn hx2ynn]
-    unfold soCone
-    simp [sqrt_le_iff]
-    split_ands
-    { apply add_nonneg (le_of_lt hy)
-      exact div_nonneg (pow_two_nonneg x) (le_of_lt hy) }
-    { have hlhs : (y - x ^ 2 / y) ^ 2 + (2 * x) ^ 2
-                = (y ^ 2 + (x ^ 2 / y) ^ 2) + (-(2 * x ^ 2 * (y / y)) + 4 * x ^ 2) := by
-        norm_cast; ring
-      have hrhs : (y + x ^ 2 / y) ^ 2
-                = (y ^ 2 +  (x ^ 2 / y) ^ 2) + (2 * x ^ 2 * (y / y)) := by
-        norm_cast; ring
-      simp only [←rpow_two]
-      rw [hlhs, hrhs, div_self (ne_of_gt hy)]
-      apply add_le_add (le_refl _)
-      ring_nf!
-      exact le_refl _ })
+    rw [mul_div, mul_comm, ←mul_div, div_self (ne_of_gt hy), mul_one])
+  (c2 : by
+    have hynn := le_of_lt hy
+    rw [rpow_two]
+    exact div_nonneg (pow_two_nonneg x) hynn)
+  (c3 : by
+    simp [exp_log hy])
 optimality by
-  intros y' hyy'
-  sorry
+  intros z hyz
+  have hy := lt_of_lt_of_le (exp_pos _) c3
+  have hynn := le_of_lt hy
+  rw [soCone_add_sub_two_mul_of_nonneg x hynn c2] at c1
+  have hz := lt_of_lt_of_le hy hyz
+  rw [div_le_iff' hz]
+  apply le_trans c1
+  apply mul_le_mul hyz (le_refl _) c2 (le_of_lt hz)
 vconditionElimination
-  (hy : sorry)
+  (hy : by
+    intros z hz
+    have hy := lt_of_lt_of_le (exp_pos _) c3
+    exact lt_of_lt_of_le hy hz)
 
 end CvxLean
