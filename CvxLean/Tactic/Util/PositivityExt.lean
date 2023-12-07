cf3a8ce98bc3c364b4ac8330106dace41a9368d3
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-12-07 16:18:18 -0500

    feat: extend `prepare_positivity`

--- CvxLean/Tactic/Util/PositivityExt.lean
+++ CvxLean/Tactic/Util/PositivityExt.lean
@@ -14,28 +14,54 @@ elab (name := cases_and) "cases_and" : tactic => do
 
 def preparePositivity (mvarId : MVarId) : MetaM MVarId := do
   mvarId.withContext do
+    -- Adjust hypotheses if needed.
     let mut hyps := #[]
+    let le_lemmas := [``sub_nonneg_of_le, ``neg_nonneg_of_nonpos]
+    let lt_lemmas := [``sub_pos_of_lt, ``neg_pos_of_neg]
     let mut lctx ← getLCtx
     for localDecl in lctx do
       let ty := localDecl.type
-      let le_lemma := ``sub_nonneg_of_le
-      let lt_lemma := ``sub_pos_of_lt
-      for (le_or_lt, le_or_lt_lemma) in [(``LE.le, le_lemma), (``LT.lt, lt_lemma)] do
+      for (le_or_lt, lemmas) in [(``LE.le, le_lemmas), (``LT.lt, lt_lemmas)] do
         let ty := Expr.consumeMData ty
         match ty.app4? le_or_lt with
-        | some (R, _, lhs, _rhs) =>
-            if ← isDefEq R q(ℝ) then
-              if ← isDefEq lhs q(0 : ℝ) then
-                continue
-              -- If LHS is not zero, add new hypothesis.
-              let val ← mkAppM le_or_lt_lemma #[localDecl.toExpr]
-              let ty ← inferType val
-              let n := localDecl.userName
-              hyps := hyps.push (Hypothesis.mk n ty val)
+        | some (R, _, lhs, rhs) =>
+            if !(← isDefEq R q(ℝ)) then
+              continue
+            if ← isDefEq lhs q(0 : ℝ) then
+              continue
+            let le_or_lt_lemma :=
+              if ← isDefEq rhs q(0 : ℝ) then lemmas[1]! else lemmas[0]!
+            -- If LHS is not zero, add new hypothesis.
+            let val ← mkAppM le_or_lt_lemma #[localDecl.toExpr]
+            let ty ← inferType val
+            let n := localDecl.userName
+            hyps := hyps.push (Hypothesis.mk n ty val)
         | none => continue
 
-    let (_, newMVarId) ← mvarId.assertHypotheses hyps
-    return newMVarId
+    let (_, mvarId) ← mvarId.assertHypotheses hyps
+
+    -- Adjust goal if needed.
+    let goalExpr ← mvarId.getType
+    let mut mvarId := mvarId
+    let le_lemmas := [``le_of_sub_nonneg, ``nonpos_of_neg_nonneg]
+    let lt_lemmas := [``lt_of_sub_pos, ``neg_of_neg_pos]
+    for (le_or_lt, lemmas) in [(``LE.le, le_lemmas), (``LT.lt, lt_lemmas)] do
+      match goalExpr.app4? le_or_lt with
+        | some (R, _, lhs, rhs) =>
+            if !(← isDefEq R q(ℝ)) then
+              continue
+            if ← isDefEq lhs q(0 : ℝ) then
+              continue
+            let le_or_lt_lemma :=
+              if ← isDefEq rhs q(0 : ℝ) then lemmas[1]! else lemmas[0]!
+            if let [g] ← mvarId.applyConst le_or_lt_lemma then
+              mvarId := g
+              break
+            else
+              throwError "prepare_positivity failed"
+        | none => continue
+
+    return mvarId
 
 elab (name := prepare_positivity) "prepare_positivity" : tactic => do
   let mvarId ← getMainGoal
