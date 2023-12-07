8eb7148d5a035b387231b2a10d0e14afe36efdfe
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-12-07 17:13:48 -0500

    wip: solving hypersonic shape but wrong value

--- CvxLean/Test/Convexify/DQCP.lean
+++ CvxLean/Test/Convexify/DQCP.lean
@@ -51,12 +51,35 @@ def hypersonicShapeDesign (a b : ℝ) :=
       h2 : Δx ≤ 1
       h3 : a * (1 / Δx) - (1 - b) * sqrt (1 - Δx ^ 2) ≤ 0
 
-time_cmd reduction redqcp2/dqcp2 : hypersonicShapeDesign 0.35 0.65 := by
+time_cmd reduction redqcp2/dqcp2 : hypersonicShapeDesign 0.05 0.65 := by
   unfold hypersonicShapeDesign;
   convexify
-  dcp --TODO: This has 0 ≤ Δx ?????
+  dcp
 
--- solve dqcp2
+solve dqcp2
+
+#print dqcp2
+-- def dqcp2 : Minimization (ℝ × ℝ × ℝ × ℝ × ℝ × ℝ × ℝ × ℝ) ℝ :=
+-- optimization (Δx : ℝ) (t₀.0 : ℝ) (t₁.1 : ℝ) (t.2 : ℝ) (y'.3 : ℝ) (t.0.4 : ℝ) (t.5 : ℝ) (t.6 : ℝ)
+--   minimize t₀.0 - 1
+--   subject to
+--     _ : posOrthCone (1 - Δx)
+--     _ : posOrthCone (0 - 7 / 20 * (t.2 - t.6))
+--     _ : soCone (Δx + t₁.1) ![Δx - t₁.1, 2]
+--     _ : soCone (t₀.0 + 1) ![t₀.0 - 1, 2 * t₁.1]
+--     _ : posOrthCone (t₀.0 - 0)
+--     _ : posOrthCone (t₁.1 - 0)
+--     _ : posOrthCone (Δx - 0)
+--     _ : soCone (Δx + t.2) ![Δx - t.2, 2 * 1]
+--     _ : posOrthCone (t.2 - 0)
+--     _ : posOrthCone (Δx - t.0.4)
+--     _ : expCone y'.3 1 t.0.4
+--     _ : rotatedSoCone t.5 (1 / 2) ![Δx]
+--     _ : rotatedSoCone (1 - t.5) (1 / 2) ![t.6]
+
+-- TODO: this is wrong.
+#eval dqcp2.value
+#eval dqcp2.solution
 
 end QCP2
 
