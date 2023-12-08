827c296aafcd2f86ef7d9a3b2fe3350ac114e599
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-12-08 17:49:11 -0500

    wip: correct solution for hypersonic shape design

--- CvxLean/Test/Convexify/DQCP.lean
+++ CvxLean/Test/Convexify/DQCP.lean
@@ -43,23 +43,40 @@ end QCP1
 /- -/
 section QCP2
 
+-- def hypersonicShapeDesign (a b : ℝ) :=
+--   optimization (Δx : ℝ)
+--     minimize sqrt (1 / (Δx ^ 2) - 1)
+--     subject to
+--       h1 : 0 < Δx
+--       h2 : Δx ≤ 1
+--       h3 : a * (1 / Δx) - (1 - b) * sqrt (1 - Δx ^ 2) ≤ 0
+
+-- time_cmd equivalence redqcp2/dqcp2 : hypersonicShapeDesign 0.05 0.65 := by
+--   unfold hypersonicShapeDesign
+--   convexify
+
+-- TODO: the issue comes from the redundant exp constraint that we use
+-- as a positivity trick.
+
 def hypersonicShapeDesign (a b : ℝ) :=
   optimization (Δx : ℝ)
-    minimize sqrt (1 / (Δx ^ 2) - 1)
+    minimize Δx ^ (-2) - 1
     subject to
       h1 : 0 < Δx
       h2 : Δx ≤ 1
-      h3 : a * (1 / Δx) - (1 - b) * sqrt (1 - Δx ^ 2) ≤ 0
+      h3 : (1/7) * (1 / Δx) ≤ sqrt (1 - Δx ^ 2)
 
-time_cmd reduction redqcp2/dqcp2 : hypersonicShapeDesign 0.05 0.65 := by
-  unfold hypersonicShapeDesign;
-  convexify
-  dcp
+time_cmd equivalence redqcp2/dqcp2 : hypersonicShapeDesign 0.05 0.65 := by
+  unfold hypersonicShapeDesign
 
+-- TODO: Error in solve:
+-- Lean server printed an error:
+-- PANIC at Lean.Expr.mvarId! Lean.Expr:1067:14: mvar expected
+-- PANIC at Lean.MetavarContext.getDecl Lean.MetavarContext:398:17: unknown metavariable
 solve dqcp2
 
-#print dqcp2
--- def dqcp2 : Minimization (ℝ × ℝ × ℝ × ℝ × ℝ × ℝ × ℝ × ℝ) ℝ :=
+#print dqcp2.reduced
+-- def dqcp2.reduced : Minimization (ℝ × ℝ × ℝ × ℝ × ℝ × ℝ × ℝ × ℝ) ℝ :=
 -- optimization (Δx : ℝ) (t₀.0 : ℝ) (t₁.1 : ℝ) (t.2 : ℝ) (y'.3 : ℝ) (t.0.4 : ℝ) (t.5 : ℝ) (t.6 : ℝ)
 --   minimize t₀.0 - 1
 --   subject to
@@ -76,6 +93,9 @@ solve dqcp2
 --     _ : expCone y'.3 1 t.0.4
 --     _ : rotatedSoCone t.5 (1 / 2) ![Δx]
 --     _ : rotatedSoCone (1 - t.5) (1 / 2) ![t.6]
+
+
+    -- _ : posOrthCone (t.6 - t.2 / 20 / (7 / 20))
 
 -- TODO: this is wrong.
 #eval dqcp2.value
