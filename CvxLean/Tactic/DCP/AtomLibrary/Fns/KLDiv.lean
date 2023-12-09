1be6d20b954c57c92fb7b6ca01ab33775ba2603f
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-12-09 17:09:41 -0500

    fix: remove `exp y' ≤ y` from everywhere

--- CvxLean/Tactic/DCP/AtomLibrary/Fns/KLDiv.lean
+++ CvxLean/Tactic/DCP/AtomLibrary/Fns/KLDiv.lean
@@ -10,18 +10,18 @@ namespace CvxLean
 
 open Real
 
+-- TODO(RFM): same issue as in `quadOverLin`, no clear way to have 0 < y as a
+-- vcondition, the exp y' ≤ y trick makes the solver stall.
 declare_atom klDiv [convex] (x : ℝ)? (y : ℝ)? : x * log (x / y) - x + y  :=
 vconditions
   (hx : 0 ≤ x)
-  (hy : 0 < y)
-implementationVars (t : ℝ) (y' : ℝ)
+  (hy : 10e-6 ≤ y)
+implementationVars (t : ℝ)
 implementationObjective (y - x - t)
 implementationConstraints
   (c1 : expCone t x y)
-  -- NOTE(RFM): This is a trick to make y strictly positive.
-  -- TODO(RFM): This can make the solver stall (error 166).
-  (c2 : exp y' ≤ y)
-solution (t := -(x * log (x / y))) (y' := log y)
+  (c2 : 10e-6 ≤ y)
+solution (t := -(x * log (x / y)))
 solutionEqualsAtom by
   ring
 feasibility
@@ -32,15 +32,17 @@ feasibility
     | inl hx =>
         left
         refine ⟨hx, ?_⟩
+        have hypos : 0 < y := by positivity
         rw [mul_comm _ (log _), ←neg_mul, ←mul_div, div_self (ne_of_gt hx)]
-        rw [mul_one, exp_neg, exp_log (div_pos hx hy), inv_div, mul_div]
+        rw [mul_one, exp_neg, exp_log (div_pos hx hypos), inv_div, mul_div]
         rw [div_le_iff hx, mul_comm]
     | inr hx =>
         replace hx := hx.symm
         right
-        simp [hx, le_of_lt hy])
+        simp [hx]
+        positivity)
   (c2 : by
-      simp [exp_log hy])
+      exact hy)
 optimality by
   unfold expCone at c1
   cases c1 with
@@ -66,18 +68,18 @@ vconditionElimination
     | inl c => exact le_of_lt c.1
     | inr c => rw [c.1])
   (hy : by
-    exact lt_of_lt_of_le (exp_pos _) c2)
+    exact c2)
 
 declare_atom klDiv2 [convex] (x : ℝ)? (y : ℝ)? : klDiv x y :=
 vconditions
   (hx : 0 ≤ x)
-  (hy : 0 < y)
-implementationVars (t : ℝ) (y' : ℝ)
+  (hy : 10e-6 ≤ y)
+implementationVars (t : ℝ)
 implementationObjective (y - x - t)
 implementationConstraints
   (c1 : expCone t x y)
-  (c2 : exp y' ≤ y)
-solution (t := -(x * log (x / y))) (y' := log y)
+  (c2 : 10e-6 ≤ y)
+solution (t := -(x * log (x / y)))
 solutionEqualsAtom by
   unfold klDiv
   ring
@@ -89,36 +91,30 @@ feasibility
     unfold posOrthCone at h
     simpa using h)
 optimality by
-  apply klDiv.optimality x y t y' (exp y') c1
+  apply klDiv.optimality x y t c1
   { unfold posOrthCone expCone at *
     simpa using c2 }
-  { unfold expCone at *
-    simp }
 vconditionElimination
   (hx : by
-    apply klDiv.vcondElim0 x y t y' (exp y') c1
-    { unfold posOrthCone expCone at *
-      simpa using c2 }
-    { unfold expCone at *
-      simp })
+    apply klDiv.vcondElim0 x y t c1
+    unfold posOrthCone at *
+    simpa using c2)
   (hy : by
-    apply klDiv.vcondElim1 x y t y' (exp y') c1
-    { unfold posOrthCone expCone at *
-      simpa using c2 }
-    { unfold expCone at *
-      simp })
+    apply klDiv.vcondElim1 x y t c1
+    unfold posOrthCone expCone at *
+    simpa using c2)
 
 declare_atom Vec.klDiv [convex] (m : Nat)& (x : Fin m → ℝ)? (y : Fin m → ℝ)? :
   Vec.klDiv x y :=
 vconditions
   (hx : 0 ≤ x)
-  (hy : ∀ i, 0 < y i)
-implementationVars (t : Fin m → ℝ) (y' : Fin m → ℝ)
+  (hy : (fun _ => 10e-6) ≤ y)
+implementationVars (t : Fin m → ℝ)
 implementationObjective (y - x - t)
 implementationConstraints
   (c1 : Vec.expCone t x y)
-  (c2 : Vec.exp y' ≤ y)
-solution (t := fun i => -((x i) * log ((x i) / (y i)))) (y' := Vec.log y)
+  (c2 : Vec.const m 10e-6 ≤ y)
+solution (t := fun i => -((x i) * log ((x i) / (y i))))
 solutionEqualsAtom by
   simp [Vec.klDiv, klDiv]; ext i; simp; ring
 feasibility
@@ -135,25 +131,19 @@ feasibility
     simpa using h)
 optimality fun i => by
     unfold Vec.expCone at c1
-    apply klDiv.optimality (x i) (y i) (t i) (y' i) (exp (y' i)) (c1 i)
-    { unfold posOrthCone expCone at *
-      simpa using (c2 i) }
-    { unfold expCone at *
-      simp }
+    apply klDiv.optimality (x i) (y i) (t i) (c1 i)
+    unfold posOrthCone expCone at *
+    simpa using (c2 i)
 vconditionElimination
   (hx : fun i => by
     unfold Vec.expCone at c1
-    apply klDiv.vcondElim0 (x i) (y i) (t i) (y' i) (exp (y' i)) (c1 i)
-    { unfold posOrthCone expCone at *
-      simpa using (c2 i) }
-    { unfold expCone at *
-      simp })
+    apply klDiv.vcondElim0 (x i) (y i) (t i) (c1 i)
+    unfold posOrthCone expCone at *
+    simpa using (c2 i))
   (hy : fun i => by
     unfold Vec.expCone at c1
-    apply klDiv.vcondElim1 (x i) (y i) (t i) (y' i) (exp (y' i)) (c1 i)
-    { unfold posOrthCone expCone at *
-      simpa using (c2 i) }
-    { unfold expCone at *
-      simp })
+    apply klDiv.vcondElim1 (x i) (y i) (t i) (c1 i)
+    unfold posOrthCone expCone at *
+    simpa using (c2 i))
 
 end CvxLean
--- CvxLean/Tactic/DCP/AtomLibrary/Fns/QuadOverLin.lean
+++ CvxLean/Tactic/DCP/AtomLibrary/Fns/QuadOverLin.lean
@@ -14,35 +14,35 @@ open Real
 
 -- TODO(RFM): generalize to x : Fin n → ℝ
 -- TODO(RFM): distinguish between nonincreasing and nondecreasing.
+-- TODO(RFM): how to do strict positivity and make vcondelimination work?
 declare_atom quadOverLin [convex] (x : ℝ)? (y : ℝ)- : x ^ 2 / y :=
 vconditions
-  (hy : 0 < y)
+  (hy : 10e-6 ≤ y)
 implementationVars (t : ℝ) (y' : ℝ)
 implementationObjective (t)
 implementationConstraints
   (c1 : soCone (y + t) ![y - t, 2 * x])
   (c2 : 0 ≤ t)
-  -- NOTE(RFM): This is a trick to make y strictly positive.
-  -- TODO(RFM): This makes the solver stall (error 166).
-  -- (c3 : exp y' ≤ y)
+  (c3 : 10e-6 ≤ y)
 solution (t := x ^ 2 / y) (y' := log y)
 solutionEqualsAtom by rfl
 feasibility
   (c1 : by
-    have hynn := le_of_lt hy
+    have hypos : 0 < y := by positivity
+    have hynn : 0 ≤ y := by linarith
     have hx2ynn : 0 ≤ x ^ 2 / y := by
       rw [rpow_two]; exact div_nonneg (pow_two_nonneg x) hynn
     rw [soCone_add_sub_two_mul_of_nonneg _ hynn hx2ynn]
-    rw [mul_div, mul_comm, ←mul_div, div_self (ne_of_gt hy), mul_one])
+    rw [mul_div, mul_comm, ←mul_div, div_self (ne_of_gt hypos), mul_one])
   (c2 : by
-    have hynn := le_of_lt hy
+    have hynn : 0 ≤ y := by positivity
     rw [rpow_two]
     exact div_nonneg (pow_two_nonneg x) hynn)
-  -- (c3 : by
-  --   simp [exp_log hy])
+  (c3 : by
+    exact hy)
 optimality by
   intros z hyz
-  have hy : 0 < y := sorry --lt_of_lt_of_le (exp_pos _) c3
+  have hy : 0 < y := by positivity
   have hynn := le_of_lt hy
   rw [soCone_add_sub_two_mul_of_nonneg x hynn c2] at c1
   have hz := lt_of_lt_of_le hy hyz
@@ -51,9 +51,7 @@ optimality by
   apply mul_le_mul hyz (le_refl _) c2 (le_of_lt hz)
 vconditionElimination
   (hy : by
-    -- intros z hz
-    -- have hy := lt_of_lt_of_le (exp_pos _) c3
-    -- exact lt_of_lt_of_le hy hz
-    sorry)
+    intros z hz
+    linarith)
 
 end CvxLean
