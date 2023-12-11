f4c68e4b09b17b64f0ebc77009b89e037d1b28c9
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-12-11 18:04:01 -0500

    test: add quiz tests to test suite

--- CvxLean/Test/Convexify/All.lean
+++ CvxLean/Test/Convexify/All.lean
@@ -1,5 +1,6 @@
-import CvxLean.Test.Convexify.MainExample
 import CvxLean.Test.Convexify.Unit
-import CvxLean.Test.Convexify.DGP
 import CvxLean.Test.Convexify.AlmostDGP
+import CvxLean.Test.Convexify.DGP
 import CvxLean.Test.Convexify.DQCP
+import CvxLean.Test.Convexify.MainExample
+import CvxLean.Test.Convexify.Quiz
