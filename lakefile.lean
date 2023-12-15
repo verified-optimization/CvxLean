700c909da2a16bc51a547e5459899a6560f9d2b0
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2023-12-15 13:17:39 -0500

    chore: bump to v4.4.0-rc1

--- lakefile.lean
+++ lakefile.lean
@@ -3,8 +3,9 @@ open System Lake DSL
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @
-  "81dd376a02781030ead59ee35ca5334a7fccc527"
+  "1250aa83953a2c7d5819cebea08ad7fdef997d49"
 
+meta if get_config? env = some "dev" then
 require scilean from git
   "https://github.com/verified-optimization/SciLean" @
   "master"
