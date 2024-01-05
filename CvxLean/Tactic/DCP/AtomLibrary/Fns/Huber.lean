8a9758669b4321499e44223c0450f90775916b31
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-01-05 11:57:55 +0100

    feat: minimal implementation constraints for Huber

--- CvxLean/Tactic/DCP/AtomLibrary/Fns/Huber.lean
+++ CvxLean/Tactic/DCP/AtomLibrary/Fns/Huber.lean
@@ -20,9 +20,8 @@ implementationVars (v : ℝ) (w : ℝ)
 implementationObjective (2 * v + w ^ (2 : ℝ))
 implementationConstraints
   (c1 : |x| ≤ v + w)
-  (c2 : 0 ≤ w)
+  (c2 : 0 ≤ v)
   (c3 : w ≤ 1)
-  (c4 : 0 ≤ v)
 solution
   (v := if |x| ≤ 1 then 0 else |x| - 1)
   (w := if |x| ≤ 1 then |x| else 1)
@@ -33,10 +32,8 @@ feasibility
   (c1 : by
     split_ifs <;> linarith)
   (c2 : by
-    split_ifs <;> norm_num)
-  (c3 : by
     split_ifs <;> linarith)
-  (c4 : by
+  (c3 : by
     split_ifs <;> linarith)
 optimality by
   simp [huber]
@@ -47,11 +44,12 @@ optimality by
       rw [←sub_le_iff_le_add, ←sq_abs, ←rpow_two, ←rpow_two, hsq]
       apply mul_le_mul <;> linarith }
     { replace hwx := not_le.mp hwx
+      have hw : 0 ≤ w := by linarith [abs_nonneg x]
       by_cases hxz : x = 0
       { simp [hxz]
-        apply add_nonneg <;> norm_num [c2, c4] }
+        apply add_nonneg <;> norm_num [c2, hw] }
       have hcx : 0 < |x| := abs_pos.mpr hxz
-      have hx2w2 := mul_lt_mul hwx (le_of_lt hwx) hcx c2
+      have hx2w2 := mul_lt_mul hwx (le_of_lt hwx) hcx hw
       rw [←pow_two, sq_abs, ←pow_two] at hx2w2
       have hv : 0 ≤ 2 * v := by linarith
       have hx2w22v := add_le_add (le_of_lt hx2w2) hv
@@ -71,9 +69,8 @@ implementationVars (v : Fin m → ℝ) (w : Fin m → ℝ)
 implementationObjective (2 • v + w ^ (2 : ℝ))
 implementationConstraints
   (c1 : |x| ≤ v + w)
-  (c2 : 0 ≤ w)
+  (c2 : 0 ≤ v)
   (c3 : w ≤ 1)
-  (c4 : 0 ≤ v)
 solution
   (v := fun i => if |x i| ≤ 1 then 0 else |x i| - 1)
   (w := fun i => if |x i| ≤ 1 then |x i| else 1)
@@ -99,12 +96,6 @@ feasibility
     have h := huber.feasibility2 (x i)
     unfold Real.posOrthCone at h
     simpa using h)
-  (c4 : by
-    dsimp
-    intros i
-    have h := huber.feasibility3 (x i)
-    unfold Real.posOrthCone at h
-    simpa using h)
 optimality by
     intros i
     simp [Vec.huber]
@@ -113,7 +104,6 @@ optimality by
     { unfold posOrthCone; simpa using c1 i }
     { unfold posOrthCone; simpa using c2 i }
     { unfold posOrthCone; simpa using c3 i }
-    { unfold posOrthCone; simpa using c4 i }
     { unfold rotatedSoCone; simp [sq_nonneg]; norm_num }
 
 vconditionElimination
