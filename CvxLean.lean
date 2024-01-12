09562bce25a2de70f9fdbfcbc3cca6202e3290e1
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-01-12 11:45:41 +0000

    fix: change main file for CI

--- CvxLean.lean
+++ CvxLean.lean
@@ -2,5 +2,4 @@ import CvxLean.Command.Solve
 import CvxLean.Command.Reduction
 import CvxLean.Command.Equivalence
 import CvxLean.Tactic.Basic.All
-import CvxLean.Tactic.DCP.DCP
 import CvxLean.Tactic.PreDCP.PreDCP
