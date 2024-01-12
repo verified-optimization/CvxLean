087f94d1e35eab918c7a90458af1cae9d56bceec
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-01-12 11:38:02 +0000

    chore: re-order modules

--- CvxLean.lean
+++ CvxLean.lean
@@ -2,5 +2,5 @@ import CvxLean.Command.Solve
 import CvxLean.Command.Reduction
 import CvxLean.Command.Equivalence
 import CvxLean.Tactic.Basic.All
-import CvxLean.Tactic.PreDCP.PreDCP
 import CvxLean.Tactic.DCP.DCP
+import CvxLean.Tactic.PreDCP.PreDCP
