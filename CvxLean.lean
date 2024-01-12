fe7d97d367136b733a8bed51e9587e056fdc9a4f
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-01-12 12:49:50 +0000

    fix: build DCP tactic first

--- CvxLean.lean
+++ CvxLean.lean
@@ -1,5 +1,6 @@
+import CvxLean.Tactic.DCP.DCP
+import CvxLean.Tactic.Basic.All
+import CvxLean.Tactic.PreDCP.PreDCP
 import CvxLean.Command.Solve
 import CvxLean.Command.Reduction
 import CvxLean.Command.Equivalence
-import CvxLean.Tactic.Basic.All
-import CvxLean.Tactic.PreDCP.PreDCP
