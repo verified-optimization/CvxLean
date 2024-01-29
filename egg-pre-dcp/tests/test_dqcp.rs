db27190cc1ee65ff7c127562d6f5e848ae3c3440
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-01-29 17:20:18 -0500

    test: add egg test with params

--- egg-pre-dcp/tests/test_dqcp.rs
+++ egg-pre-dcp/tests/test_dqcp.rs
@@ -28,3 +28,16 @@ fn test_qcp2() {
             "(le (sub (mul (div 1 20) (div 1 (var x))) (mul (div 7 20) (sqrt (sub 1 (pow (var x) 2))))) 0)"
         ])
 }
+
+#[test]
+fn test_qcp2_with_params() {
+    let dx = Domain::make_oc(domain::zero(), domain::one());
+    let da = Domain::make_ci(domain::zero());
+    let db = Domain::make_io(domain::one());
+    convexify_check_with_domain(
+        vec![("x", dx), ("a", da), ("b", db)], 
+        "(sqrt (sub (div 1 (pow (var x) 2)) 1))",
+        vec![
+            "(le (sub (mul (param a) (div 1 (var x))) (mul (sub 1 (param b)) (sqrt (sub 1 (pow (var x) 2))))) 0)"
+        ])
+}
