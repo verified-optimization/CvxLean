662ad7a230434fa6fa9b3a9e734817c94b7c9f12
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-02-01 17:40:24 -0500

    doc: `Command/Solve.lean`

--- CvxLean/Command/Solve.lean
+++ CvxLean/Command/Solve.lean
@@ -4,20 +4,24 @@ import CvxLean.Command.Solve.Conic
 /-!
 # The `solve` command
 
-
+Given a problem `p : Minimization D R` a user can call `solve p` to call an external solver and
+obtain a result. If successful, three new definitions are added to the environment:
+* `p.reduced : Minimization (D × E) R` is the problem in conic form.
+* `p.status : String` is the status of the solution.
+* `p.solution : D` is the solution to the problem.
+* `p.value : R` is the value of the solution.
 -/
 
 namespace CvxLean
 
-open Lean Lean.Elab Lean.Elab.Term Lean.Elab.Command Lean.Meta
-
-open Minimization
+open Lean Meta Elab Term Command Minimization
 
 /-- Get problem name. Used to add information about the solution to the environment. -/
-def getProblemName (term : Syntax) : MetaM Lean.Name := do
-  -- TODO: Full name with parameters.
-  let idStx := match term with
-    | Syntax.ident _ _ _ _ => term
+def getProblemName (stx : Syntax) : MetaM Name := do
+  -- TODO: Full name with parameters?
+  let idStx :=
+    match stx with
+    | Syntax.ident _ _ _ _ => stx
     | Syntax.node _ _ args => args.getD 0 Syntax.missing
     | _ => Syntax.missing
   if ¬ idStx.getId.isStr then
@@ -25,17 +29,21 @@ def getProblemName (term : Syntax) : MetaM Lean.Name := do
 
   return idStx.getId
 
-/-- -/
-def getReducedProblemAndBwdMap (prob : Expr) : MetaM (Meta.MinimizationExpr × Expr) := do
-  let ogProb ← Meta.MinimizationExpr.fromExpr prob
+/-- Call DCP and get the problem in conic form as well as `ψ`, the backward map from the
+equivalence. -/
+def getReducedProblemAndBwdMap (prob : Expr) : MetaM (MinimizationExpr × Expr) := do
+  let ogProb ← MinimizationExpr.fromExpr prob
   let (redProb, eqvProof) ← DCP.canonize ogProb
   let backwardMap ← mkAppM ``Minimization.Equivalence.psi #[eqvProof]
   return (redProb, backwardMap)
 
 syntax (name := solve) "solve " term : command
 
--- set_option maxHeartbeats 1000000
-
+/-- The `solve` command. It works as follows:
+1. Reduce optimization problem to conic form.
+2. Extract problem data using `determineCoeffsFromExpr`.
+3. Obtain a solution using `solutionDataFromProblemData`, which calls an external solver.
+4. Store the result in the enviroment. -/
 @[command_elab «solve»]
 unsafe def evalSolve : CommandElab := fun stx =>
   match stx with
@@ -62,53 +70,44 @@ unsafe def evalSolve : CommandElab := fun stx =>
 
       -- Call the solver on prob.reduced and get a point in E.
       let (coeffsData, sections) ← determineCoeffsFromExpr redProb
-      trace[Meta.debug] "coeffsData: {coeffsData}"
+      trace[CvxLean.debug] "Coeffs data:\n{coeffsData}"
 
-      let solPointResponse ← Meta.conicSolverFromValues redProb coeffsData sections
-      trace[Meta.debug] "solPointResponse: {solPointResponse}"
+      let solData ← solutionDataFromProblemData redProb coeffsData sections
+      trace[CvxLean.debug] "Solution data:\n{solData}"
 
-      match solPointResponse with
-      | Sol.Response.failure code =>
-          trace[Meta.debug] "MOSEK failed with code {code}"
-          pure ()
-      -- Mosek finished successfully.
-      | Sol.Response.success solPoint =>
-          trace[Meta.debug] "solPoint.summary: {solPoint.summary}"
+      -- Add status to the environment.
+      simpleAddAndCompileDefn (probName ++ `status) (mkStrLit solData.status)
 
-          -- Add status to the environment.
-          simpleAddAndCompileDefn (probName ++ `status) (mkStrLit solPoint.summary.problemStatus)
+      -- TODO: For now, we are only handling this case.
+      if solData.status != "PRIMAL_AND_DUAL_FEASIBLE" then
+        pure ()
 
-          -- TODO: For now, we are only handling this case.
-          if solPoint.summary.problemStatus != "PRIMAL_AND_DUAL_FEASIBLE" then
-            pure ()
+      -- Solution makes sense, handle the numerical solution.
+      let solPointExpr ← exprFromSolutionData redProb solData
+      trace[CvxLean.debug] "Solution point (reduced problem): {solPointExpr}"
 
-          -- Solution makes sense, handle the numerical solution.
-          let solPointExpr ← Meta.exprFromSol redProb solPoint
-          trace[Meta.debug] "solPointExpr: {solPointExpr}"
+      let backwardMapFloat ← realToFloat <| ← whnf backwardMap
+      let solPointExprFloat ← realToFloat solPointExpr
 
-          let backwardMapFloat ← realToFloat <| ← whnf backwardMap
-          let solPointExprFloat ← realToFloat solPointExpr
+      let probSolPointFloat ← whnf <| mkAppN backwardMapFloat #[solPointExprFloat]
+      trace[CvxLean.debug] "Float solution point (original problem): {probSolPointFloat}"
 
-          let probSolPointFloat ← whnf <| mkAppN backwardMapFloat #[solPointExprFloat]
-          trace[Meta.debug] "probSolPointFloat: {probSolPointFloat}"
+      -- Add the solution point to the environment.
+      simpleAddAndCompileDefn (probName ++ `solution) probSolPointFloat
 
-          -- Add the solution point to the environment.
-          simpleAddAndCompileDefn (probName ++ `solution) probSolPointFloat
+      -- Also add value of optimal point.
+      let probSolValue := mkApp redProb.objFun solPointExpr
+      let probSolValueFloat ← realToFloat probSolValue
+      check probSolValueFloat
 
-          -- Also add value of optimal point.
-          let probSolValue := mkApp redProb.objFun solPointExpr
-          let probSolValueFloat ← realToFloat probSolValue
-          trace[Meta.debug] "probSolValueFloat {probSolValueFloat}"
-          check probSolValueFloat
+      let mut probSolValueFloat := Expr.headBeta probSolValueFloat
+      trace[CvxLean.debug] "Float problem value (original problem): {probSolValueFloat}"
 
-          let mut probSolValueFloat := Expr.headBeta probSolValueFloat
-          trace[Meta.debug] "probSolValueFloat reduced: {probSolValueFloat}"
+      if probSolValueFloat.getAppFn.isConstOf `CvxLean.maximizeNeg then
+        probSolValueFloat := probSolValueFloat.getAppArgs[2]!
+      simpleAddAndCompileDefn (probName ++ `value) probSolValueFloat
 
-          if probSolValueFloat.getAppFn.isConstOf `CvxLean.maximizeNeg then
-            probSolValueFloat := probSolValueFloat.getAppArgs[2]!
-          simpleAddAndCompileDefn (probName ++ `value) probSolValueFloat
-
-          pure ()
+      pure ()
   | _ => throwUnsupportedSyntax
 
 end CvxLean
