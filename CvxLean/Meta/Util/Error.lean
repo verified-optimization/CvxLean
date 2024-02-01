d1d15ceff12b427cbf01da202af1c083ae022052
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-02-01 18:35:56 -0500

    feat: more custom error commands

--- CvxLean/Meta/Util/Error.lean
+++ CvxLean/Meta/Util/Error.lean
@@ -4,6 +4,18 @@ import Lean
 Custom error messages.
 -/
 
+syntax "throwPreDCPError " (interpolatedStr(term) <|> term) : term
+
+macro_rules
+  | `(throwPreDCPError $msg:interpolatedStr) => `(throwError ("`pre_dcp` error: " ++ (m! $msg)))
+  | `(throwPreDCPError $msg:term) => `(throwError ("`pre_dcp` error: " ++ $msg))
+
+syntax "throwDCPError " (interpolatedStr(term) <|> term) : term
+
+macro_rules
+  | `(throwDCPError $msg:interpolatedStr) => `(throwError ("`dcp` error: " ++ (m! $msg)))
+  | `(throwDCPError $msg:term) => `(throwError ("`dcp` error: " ++ $msg))
+
 syntax "throwCoeffsError " (interpolatedStr(term) <|> term) : term
 
 macro_rules
@@ -30,3 +42,19 @@ macro_rules
   | `(throwEquivalenceError $msg:interpolatedStr) =>
     `(throwError ("`equivalence` error: " ++ (m! $msg)))
   | `(throwEquivalenceError $msg:term) => `(throwError ("`equivalence` error: " ++ $msg))
+
+/-- Errors in the `reduction` command. -/
+syntax "throwReductionError " (interpolatedStr(term) <|> term) : term
+
+macro_rules
+  | `(throwReductionError $msg:interpolatedStr) =>
+    `(throwError ("`reduction` error: " ++ (m! $msg)))
+  | `(throwReductionError $msg:term) => `(throwError ("`reduction` error: " ++ $msg))
+
+/-- Errors in the `relaxation` command. -/
+syntax "throwRelaxationError " (interpolatedStr(term) <|> term) : term
+
+macro_rules
+  | `(throwRelaxationError $msg:interpolatedStr) =>
+    `(throwError ("`relaxation` error: " ++ (m! $msg)))
+  | `(throwRelaxationError $msg:term) => `(throwError ("`relaxation` error: " ++ $msg))
