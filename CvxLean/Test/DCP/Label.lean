6d9a372d5cd22ccbb0ac39a99aece51ad139f18d
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-02-04 13:08:36 -0500

    fix: tests passing

--- CvxLean/Test/DCP/Label.lean
+++ CvxLean/Test/DCP/Label.lean
@@ -2,19 +2,10 @@ import CvxLean.Syntax.Label
 
 def testLabel := {** 0 ** x **}
 
-/-
-def testLabel : Nat :=
-0
--/
 #print testLabel
 
 set_option pp.CvxLean.labels true
 
-/-
-def testLabel : Nat :=
-{** 0 |x**}
--/
 #print testLabel
-
 
 #check {** Nat ** x **} × {** Nat ** y **} × {** Nat ** z**}
--- CvxLean/Test/PreDCP/Stanford.lean
+++ CvxLean/Test/PreDCP/Stanford.lean
@@ -16,22 +16,21 @@ open CvxLean Minimization Real
 section E367
 
 def e367 :=
-  optimization (x1 x2 x3 x4 x5 x6 : ℝ)
-    minimize (-(sqrt x1 + sqrt x2 + sqrt x3 + sqrt x4 + sqrt x5 + sqrt x6) ^ (2 : ℝ))
+  optimization (x1 x2 x3 x4: ℝ)
+    minimize (-(sqrt x1 + sqrt x2 + sqrt x3 + sqrt x4) ^ (2 : ℝ))
     subject to
       h1 : 0.001 ≤ x1
       h2 : 0.001 ≤ x2
       h3 : 0.001 ≤ x3
       h4 : 0.001 ≤ x4
-      h5 : 0.001 ≤ x5
-      h6 : 0.001 ≤ x6
 
-set_option maxHeartbeats 100000000
+set_option maxHeartbeats 1000000 in
 time_cmd reduction red367/dcp367 : e367 := by
   pre_dcp
-  dcp
 
 #print dcp367
+
+solve dcp367
 
 end E367
 
