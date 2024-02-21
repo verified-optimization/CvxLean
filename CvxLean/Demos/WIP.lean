d12d4683fa32724cbeb8403039cbfd82b6750333
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-02-21 19:20:21 +0000

    wip: fix equivalence proofs

--- CvxLean/Demos/WIP.lean
+++ CvxLean/Demos/WIP.lean
@@ -69,9 +69,9 @@ end CvxLean_overview
 
 namespace Equivalences
 
-def pex₁ := Minimization.mk (fun (x : ℝ) => x) (fun x => 1 ≤ x)
-def pex₂ := Minimization.mk (fun (x : ℝ) => log x) (fun x => 1 ≤ x)
-def pex₃ := Minimization.mk (fun (x : ℝ) => (log x) ^ (2 : ℝ)) (fun x => 1 ≤ x)
+def pex₁ := Minimization.mk (fun (x : ℝ) => x) (fun x => 1 ≤ x ∧ x ≤ exp 1)
+def pex₂ := Minimization.mk (fun (x : ℝ) => log x) (fun x => 1 ≤ x ∧ x ≤ exp 1)
+def pex₃ := Minimization.mk (fun (x : ℝ) => (log x) ^ (2 : ℝ)) (fun x => 1 ≤ x ∧ x ≤ exp 1)
 
 def E₁₂ : pex₁ ≡ pex₂ := Equivalence.map_objFun_log (fun x h => by positivity!)
 
@@ -79,10 +79,10 @@ def E₂₃ : pex₂ ≡ pex₃ := Equivalence.map_objFun_sq (fun x h => by posi
 
 def nS₁₂ : (pex₁ ≡' pex₂) → False :=
   fun ⟨_, psi, _, psi_feasibility, _, psi_optimality⟩ => by
-    simp [pex₁, pex₂, feasible, constraints, objFun] at *
+    simp only [pex₁, pex₂, feasible, constraints, objFun] at *
     let y : ℝ := 1
-    have h_feas_y : 1 ≤ y := by norm_num
-    have h_one_le_phiy := psi_feasibility y h_feas_y
+    have h_feas_y : 1 ≤ y ∧ y ≤ exp 1 := by norm_num
+    have h_one_le_phiy := (psi_feasibility y h_feas_y).1
     have h_psiy_le_logy := psi_optimality y h_feas_y
     have h_one_le_logy := le_trans h_one_le_phiy h_psiy_le_logy
     simp at h_one_le_logy
@@ -92,30 +92,46 @@ def nS₁₂ : (pex₁ ≡' pex₂) → False :=
 def S₂₃ : pex₂ ≡' pex₃ :=
   { phi := fun x => (exp (sqrt (log x)))
     psi := fun x => (exp ((log x) ^ 2))
-    phi_feasibility := fun x _ => by
+    phi_feasibility := fun x ⟨h1x, hx2⟩ => by
       simp [pex₂, pex₃, feasible, constraints, objFun] at *
-      exact sqrt_nonneg _,
-    psi_feasibility := fun x h_feas_x => by
+      split_ands
+      . exact sqrt_nonneg _
+      . rw [sqrt_le_iff]
+        split_ands
+        . positivity
+        . rw [← log_le_log_iff (by linarith) (by positivity)] at hx2
+          apply le_trans hx2; field_simp,
+    psi_feasibility := fun x ⟨h1x, hxe⟩ => by
       simp [pex₂, pex₃, feasible, constraints, objFun] at *
-      exact sq_nonneg _,
-    phi_optimality := fun x h_feas_x => by
+      split_ands
+      . exact sq_nonneg _
+      . rw [abs_le]
+        have hx : 0 < x := by linarith
+        rw [← log_le_log_iff (by norm_num) hx, log_one] at h1x
+        rw [← log_le_log_iff hx (exp_pos _), log_exp] at hxe
+        split_ands <;> linarith,
+    phi_optimality := fun x ⟨h1x, _⟩ => by
       simp [pex₂, pex₃, feasible, constraints, objFun] at *
-      have h_logx_nonneg : 0 ≤ log x := log_nonneg h_feas_x
+      have h_logx_nonneg : 0 ≤ log x := log_nonneg h1x
       rw [sq_sqrt h_logx_nonneg],
     psi_optimality := fun x _ => by
       simp [pex₂, pex₃, feasible, constraints, objFun] at * }
 
-def p₄ := Minimization.mk (fun (x : ℝ) => x) (fun x => 0 ≤ x)
+def p₄ := Minimization.mk (fun (x : ℝ) => x) (fun x => 0 ≤ x ∧ x ≤ 1)
 
 def S₄₂ : p₄ ≡' pex₂ :=
   { phi := fun x => exp x
     psi := fun x => log x
     phi_feasibility := fun x _ => by
       simp [p₄, pex₂, feasible, constraints, objFun] at *
-      positivity!,
+      split_ands <;> linarith,
     psi_feasibility := fun x h_feas_x => by
       simp [p₄, pex₂, feasible, constraints, objFun] at *
-      positivity!,
+      have hx : 0 < x := by linarith
+      have h_0_eq_log1 : 0 = log 1 := by simp
+      have h_1_eq_loge : 1 = log (exp 1) := by simp
+      rw [h_1_eq_loge, h_0_eq_log1]
+      rwa [log_le_log_iff hx (exp_pos _), log_le_log_iff (by norm_num) hx],
     phi_optimality := fun x _ => by
       simp [p₄, pex₂, feasible, constraints, objFun] at *,
     psi_optimality := fun x _ => by
