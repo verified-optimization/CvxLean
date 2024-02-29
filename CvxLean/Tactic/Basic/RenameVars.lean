962b3654f93bac20c5f4e65c042551edf3dc067d
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-02-29 17:35:49 +0000

    doc: rename variables

--- CvxLean/Tactic/Basic/RenameVars.lean
+++ CvxLean/Tactic/Basic/RenameVars.lean
@@ -2,13 +2,42 @@ import CvxLean.Meta.Equivalence
 import CvxLean.Meta.TacticBuilder
 import CvxLean.Meta.Util.Expr
 
+/-!
+# Tactic to rename variables
+
+This file defines the `rename_vars` tactic. It takes a list of names, e.g., `[x, y, z]`. The
+list must have the same length as the number of optimization variables in the current problem.
+
+We illustrate it with an example of how to use it inside the `equivalence` command:
+```
+equivalence eqv/q :
+    optimization (x y z : ℝ)
+      minimize x + y + z
+      subject to
+        c₁ : 1 ≤ x
+        c₂ : 1 ≤ y
+        c₃ : 1 ≤ z := by
+  rename_constrs [a, b, c]
+```
+The resulting problem `q` looks as follows:
+```
+optimization (a b c : ℝ)
+  minimize a + b + c
+  subject to
+    c₁ : 1 ≤ a
+    c₂ : 1 ≤ b
+    c₃ : 1 ≤ c
+```
+-/
+
 namespace CvxLean
 
 open Lean Expr Meta Elab Tactic
 
 namespace Meta
 
-/-- -/
+/-- Get the domain type, and replace the variables names stored in it (as meta-data) by the new
+variable names. The resulting problem is the same expression with the new domain type. -/
 def renameVarsBuilder (names : Array Name) : EquivalenceBuilder Unit := fun eqvExpr g => do
   let lhsMinExpr ← eqvExpr.toMinimizationExprLHS
   let vars ← decomposeDomain (← instantiateMVars eqvExpr.domainLHS)
@@ -46,9 +75,10 @@ end Meta
 
 namespace Tactic
 
+/-- Tactic to rename all optimization variables given a list of names. This does not change the
+mathematical interpretation of the problem. -/
 syntax (name := renameVars) "rename_vars" "[" ident,* "]" : tactic
 
-/-- -/
 @[tactic renameVars]
 partial def evalRenameVars : Tactic := fun stx => match stx with
 | `(tactic| rename_vars [$ids,*]) => do
