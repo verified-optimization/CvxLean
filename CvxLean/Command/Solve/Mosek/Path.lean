795091b335256a92faca4123309eb2829dc8ec6a
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-03-05 16:31:30 +0000

    fix: empty MOSEK path

--- CvxLean/Command/Solve/Mosek/Path.lean
+++ CvxLean/Command/Solve/Mosek/Path.lean
@@ -1,3 +1,3 @@
 -- Change this to the location of the Mosek binary, e.g.
 -- /Users/alice/mosek/10.0/tools/platform/osxaarch64/bin
-def mosekBinPath := "/Users/ramonfernandezmir/Documents/PhD-code/optimization/mosek/10.0/tools/platform/osxaarch64/bin"
+def mosekBinPath := ""
