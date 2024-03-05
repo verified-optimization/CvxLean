9102b6766c1ccda8f93b8d0d92037430b8591173
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-03-05 16:27:15 +0000

    chore: only print style errors

--- scripts/style/check_style_dir.sh
+++ scripts/style/check_style_dir.sh
@@ -9,7 +9,6 @@ fi
 
 find "$DIR" -type f -name "*.lean" -print0 | while IFS= read -r -d '' file; do
     if [ -f "$file" ]; then
-        echo "Checking style for file $file"
-        ./scripts/style/check_style_file.sh "$file" 2>&1
+        ./scripts/style/check_style_file.sh "$file"
     fi
 done
