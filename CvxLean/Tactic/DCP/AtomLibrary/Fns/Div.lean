7cc25f02d6013fbb8af23b6da4d609bd9bc72830
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-03-08 17:07:33 +0000

    feat: add vector versions of some atoms

--- CvxLean/Tactic/DCP/AtomLibrary/Fns/Div.lean
+++ CvxLean/Tactic/DCP/AtomLibrary/Fns/Div.lean
@@ -8,8 +8,9 @@ namespace CvxLean
 
 open Real
 
-declare_atom div [affine] (x : ℝ)+ (y : ℝ)& : x / y :=
-bconditions (hy : (0 : ℝ) ≤ y)
+declare_atom div1 [affine] (x : ℝ)+ (y : ℝ)& : x / y :=
+bconditions
+  (hy : 0 ≤ y)
 homogenity by
   simp [mul_div]
 additivity by
@@ -18,8 +19,21 @@ optimality by
   intros x' hx
   by_cases h : 0 = y
   · rw [← h, div_zero, div_zero]
-  · rw [div_le_div_right]
+  · rw [div_le_div_right (lt_of_le_of_ne hy h)]
     apply hx
-    apply lt_of_le_of_ne hy h
+
+declare_atom div2 [affine] (x : ℝ)- (y : ℝ)& : x / y :=
+bconditions
+  (hy : y ≤ 0)
+homogenity by
+  simp [mul_div]
+additivity by
+  simp [add_div]
+optimality by
+  intros x' hx
+  by_cases h : y = 0
+  · rw [h, div_zero, div_zero]
+  · rw [div_le_div_right_of_neg (lt_of_le_of_ne hy h)]
+    apply hx
 
 end CvxLean
