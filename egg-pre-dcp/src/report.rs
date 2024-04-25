df47c930128b02aad8aba8a7ed30388438e06b32
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-04-24 20:00:17 -0400

    wip: report structure?

---
+++ egg-pre-dcp/src/report.rs
@@ -0,0 +1,13 @@
+/*!
+Data collected for evaluation.
+!*/
+
+pub struct PreDCPComponentReport {
+    total_nodes : u32,
+    node_limit : u32,
+
+}
+
+pub struct PreDCPReport {
+    prob : String,
+}
