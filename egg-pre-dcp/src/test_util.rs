4d20b892ac7a45804fc8ccd412ddb87f54a9ff3a
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-04-25 11:04:01 -0400

    feat: keep track of problem name

--- egg-pre-dcp/src/test_util.rs
+++ egg-pre-dcp/src/test_util.rs
@@ -1,5 +1,10 @@
+use egg::{*};
+
 use crate::domain;
 use domain::Domain as Domain;
+
+use crate::optimization;
+use optimization::Optimization as Optimization;
 
 use crate::extract;
 use extract::Minimization as Minimization;
@@ -19,7 +24,12 @@ fn make(obj: &str, constrs: Vec<&str>) -> Minimization {
     };
 }
 
-fn pre_dcp_check_with_domain_maybe_print(domains : Vec<(&str, Domain)>, obj: &str, constrs: Vec<&str>, print: bool) {
+fn pre_dcp_check_with_domain_maybe_print(
+        prob_name : &str, 
+        domains : Vec<(&str, Domain)>, 
+        obj: &str, 
+        constrs: Vec<&str>, 
+        print: bool) {
     let prob = make(obj, constrs);
     let domains = 
         domains.iter().map(|(s, d)| ((*s).to_string(), d.clone())).collect();
@@ -27,10 +37,10 @@ fn pre_dcp_check_with_domain_maybe_print(domains : Vec<(&str, Domain)>, obj: &st
         match std::env::var("EGG_PRE_DCP_NODE_LIMIT") {
             Result::Ok(v) => { 
                 let node_limit = v.parse::<usize>().unwrap();
-                get_steps_maybe_node_limit(prob, domains, print, Some(node_limit)) 
+                get_steps_maybe_node_limit(prob_name, prob, domains, print, Some(node_limit)) 
             }
             Result::Err(_) => { 
-                get_steps(prob, domains, print)
+                get_steps(prob_name, prob, domains, print)
             }
         };
     if steps.is_none() {
@@ -41,28 +51,32 @@ fn pre_dcp_check_with_domain_maybe_print(domains : Vec<(&str, Domain)>, obj: &st
     }
 }
 
-pub fn pre_dcp_check_with_domain(domains : Vec<(&str, Domain)>, obj: &str, constrs: Vec<&str>) {
-    pre_dcp_check_with_domain_maybe_print(domains, obj, constrs, false)
+pub fn pre_dcp_check_with_domain(
+        prob_name : &str, domains : Vec<(&str, Domain)>, obj: &str, constrs: Vec<&str>) {
+    pre_dcp_check_with_domain_maybe_print(prob_name, domains, obj, constrs, false)
 }
 
-pub fn pre_dcp_check_with_domain_and_print(domains : Vec<(&str, Domain)>, obj: &str, constrs: Vec<&str>) {
-    pre_dcp_check_with_domain_maybe_print(domains, obj, constrs, true)
+pub fn pre_dcp_check_with_domain_and_print(
+        prob_name : &str, domains : Vec<(&str, Domain)>, obj: &str, constrs: Vec<&str>) {
+    pre_dcp_check_with_domain_maybe_print(prob_name, domains, obj, constrs, true)
 }
 
-pub fn pre_dcp_check(obj: &str, constrs: Vec<&str>) {
-    pre_dcp_check_with_domain_maybe_print(vec![], obj, constrs, false)
+pub fn pre_dcp_check(prob_name : &str, obj: &str, constrs: Vec<&str>) {
+    pre_dcp_check_with_domain_maybe_print(prob_name, vec![], obj, constrs, false)
 }
 
-pub fn pre_dcp_check_and_print(obj: &str, constrs: Vec<&str>) {
-    pre_dcp_check_with_domain_maybe_print(vec![], obj, constrs, true)
+pub fn pre_dcp_check_and_print(prob_name : &str, obj: &str, constrs: Vec<&str>) {
+    pre_dcp_check_with_domain_maybe_print(prob_name, vec![], obj, constrs, true)
 }
 
 // Used to test out-of-context expressions.
 
 fn pre_dcp_check_expression_with_domain_maybe_print_maybe_node_limit(
-    domains : Vec<(&str, Domain)>, 
-    s: &str, print: bool, 
-    node_limit: Option<usize>) {
+        prob_name : &str, 
+        domains : Vec<(&str, Domain)>, 
+        prob_s: &str, 
+        print: bool, 
+        node_limit: Option<usize>) {
     // NOTE: Even if the node limit is passed as an argument, the environment variable is used if it 
     // is set, hence overriding the argument.
     let node_limit = 
@@ -75,9 +89,10 @@ fn pre_dcp_check_expression_with_domain_maybe_print_maybe_node_limit(
             node_limit
         }
     };
+    let prob: RecExpr<Optimization> = prob_s.parse().unwrap();
     let domains = 
         domains.iter().map(|(s, d)| ((*s).to_string(), d.clone())).collect();
-    let steps = get_steps_from_string_maybe_node_limit(s, domains, true, node_limit);
+    let steps = get_steps_from_string_maybe_node_limit(prob_name, prob, domains, true, node_limit);
     if steps.is_none() {
         panic!("Test failed, could not rewrite target into DCP form.");
     }
@@ -86,34 +101,44 @@ fn pre_dcp_check_expression_with_domain_maybe_print_maybe_node_limit(
     }
 }
 
-fn pre_dcp_check_expression_with_domain_maybe_print(domains : Vec<(&str, Domain)>, s: &str, print: bool) {
-    pre_dcp_check_expression_with_domain_maybe_print_maybe_node_limit(domains, s, print, None);
+fn pre_dcp_check_expression_with_domain_maybe_print(
+        prob_name: &str, domains: Vec<(&str, Domain)>, s: &str, print: bool) {
+    pre_dcp_check_expression_with_domain_maybe_print_maybe_node_limit(
+        prob_name, domains, s, print, None);
 }
 
-pub fn pre_dcp_check_expression_with_domain(domains : Vec<(&str, Domain)>,s: &str) {
-    pre_dcp_check_expression_with_domain_maybe_print(domains, s, false);
+pub fn pre_dcp_check_expression_with_domain(
+        prob_name: &str, domains: Vec<(&str, Domain)>,s: &str) {
+    pre_dcp_check_expression_with_domain_maybe_print(prob_name, domains, s, false);
 }
 
-pub fn pre_dcp_check_expression_with_domain_and_print(domains : Vec<(&str, Domain)>,s: &str) {
-    pre_dcp_check_expression_with_domain_maybe_print(domains, s, true);
+pub fn pre_dcp_check_expression_with_domain_and_print(
+        prob_name: &str, domains: Vec<(&str, Domain)>,s: &str) {
+    pre_dcp_check_expression_with_domain_maybe_print(prob_name, domains, s, true);
 }
 
-fn pre_dcp_check_expression_with_domain_and_node_limit_maybe_print(domains : Vec<(&str, Domain)>, s: &str, print: bool, node_limit: usize) {
-    pre_dcp_check_expression_with_domain_maybe_print_maybe_node_limit(domains, s, print, Some(node_limit));
+fn pre_dcp_check_expression_with_domain_and_node_limit_maybe_print(
+        prob_name: &str, domains: Vec<(&str, Domain)>, s: &str, print: bool, node_limit: usize) {
+    pre_dcp_check_expression_with_domain_maybe_print_maybe_node_limit(
+        prob_name, domains, s, print, Some(node_limit));
 }
 
-pub fn pre_dcp_check_expression_with_domain_and_node_limit(domains : Vec<(&str, Domain)>,s: &str, node_limit: usize) {
-    pre_dcp_check_expression_with_domain_and_node_limit_maybe_print(domains, s, false, node_limit);
+pub fn pre_dcp_check_expression_with_domain_and_node_limit(
+        prob_name: &str, domains: Vec<(&str, Domain)>, s: &str, node_limit: usize) {
+    pre_dcp_check_expression_with_domain_and_node_limit_maybe_print(
+        prob_name, domains, s, false, node_limit);
 }
 
-pub fn pre_dcp_check_expression_with_domain_and_node_limit_and_print(domains : Vec<(&str, Domain)>,s: &str, node_limit: usize) {
-    pre_dcp_check_expression_with_domain_and_node_limit_maybe_print(domains, s, true, node_limit);
+pub fn pre_dcp_check_expression_with_domain_and_node_limit_and_print(
+        prob_name: &str, domains: Vec<(&str, Domain)>, s: &str, node_limit: usize) {
+    pre_dcp_check_expression_with_domain_and_node_limit_maybe_print(
+        prob_name, domains, s, true, node_limit);
 }
 
-pub fn pre_dcp_check_expression(s: &str) {
-    pre_dcp_check_expression_with_domain_maybe_print(vec![], s, false);
+pub fn pre_dcp_check_expression(prob_name: &str, s: &str) {
+    pre_dcp_check_expression_with_domain_maybe_print(prob_name, vec![], s, false);
 }
 
-pub fn pre_dcp_check_expression_and_print(s: &str) {
-    pre_dcp_check_expression_with_domain_maybe_print(vec![], s, true);
+pub fn pre_dcp_check_expression_and_print(prob_name: &str, s: &str) {
+    pre_dcp_check_expression_with_domain_maybe_print(prob_name, vec![], s, true);
 }
