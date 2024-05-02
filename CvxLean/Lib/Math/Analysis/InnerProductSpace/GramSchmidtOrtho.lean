0132023059d678b0fe2fcbdc2de2aa42bda9dad3
Ramon Fernandez Mir <rfernandezmir@gmail.com>
2024-05-02 17:32:29 -0400

    wip: fixing `Lib` errors due to new `mathlib`

--- CvxLean/Lib/Math/Analysis/InnerProductSpace/GramSchmidtOrtho.lean
+++ CvxLean/Lib/Math/Analysis/InnerProductSpace/GramSchmidtOrtho.lean
@@ -6,7 +6,7 @@ The Gram-Schmidt algorithm respects basis vectors.
 
 section GramSchmidt
 
-variable (𝕜 : Type _) {E : Type _} [IsROrC 𝕜]
+variable (𝕜 : Type _) {E : Type _} [RCLike 𝕜]
 variable [NormedAddCommGroup E] [InnerProductSpace 𝕜 E]
 variable {ι : Type _} [LinearOrder ι] [LocallyFiniteOrderBot ι]
 variable [IsWellOrder ι (· < · : ι → ι → Prop)]
@@ -17,9 +17,8 @@ local notation "⟪" x "," y "⟫" => @inner 𝕜 _ _ x y
 
 lemma repr_gramSchmidt_diagonal {i : ι} (b : Basis ι 𝕜 E) :
     b.repr (gramSchmidt 𝕜 b i) i = 1 := by
-  rw [gramSchmidt_def, LinearEquiv.map_sub, Finsupp.sub_apply, Basis.repr_self,
-    Finsupp.single_eq_same, sub_eq_self, map_sum, Finsupp.coe_finset_sum,
-    Finset.sum_apply, Finset.sum_eq_zero]
+  rw [gramSchmidt_def, map_sub, Finsupp.sub_apply, Basis.repr_self, Finsupp.single_eq_same,
+    sub_eq_self, map_sum, Finsupp.coe_finset_sum, Finset.sum_apply, Finset.sum_eq_zero]
   intros j hj
   rw [Finset.mem_Iio] at hj
   simp [orthogonalProjection_singleton, gramSchmidt_triangular hj]
--- CvxLean/Lib/Math/Analysis/InnerProductSpace/Spectrum.lean
+++ CvxLean/Lib/Math/Analysis/InnerProductSpace/Spectrum.lean
@@ -6,7 +6,7 @@ Version of the spectral theorem.
 
 namespace LinearMap
 
-variable {𝕜 : Type _} [IsROrC 𝕜] [DecidableEq 𝕜]
+variable {𝕜 : Type _} [RCLike 𝕜] [DecidableEq 𝕜]
 variable {E : Type _} [NormedAddCommGroup E] [InnerProductSpace 𝕜 E]
 variable [FiniteDimensional 𝕜 E]
 variable {n : ℕ} (hn : FiniteDimensional.finrank 𝕜 E = n)
@@ -19,14 +19,13 @@ lemma spectral_theorem' (v : E) (i : Fin n)
     (xs : OrthonormalBasis (Fin n) 𝕜 E) (as : Fin n → ℝ)
     (hxs : ∀ j, Module.End.HasEigenvector T (as j) (xs j)) :
     xs.repr (T v) i = as i * xs.repr v i := by
-  suffices : ∀ w : EuclideanSpace 𝕜 (Fin n),
-    T (xs.repr.symm w) = xs.repr.symm (fun i => as i * w i)
-  { simpa only [LinearIsometryEquiv.symm_apply_apply,
-      LinearIsometryEquiv.apply_symm_apply]
-      using congr_arg (fun (v : E) => (xs.repr) v i) (this ((xs.repr) v)) }
+  suffices hsuff : ∀ (w : EuclideanSpace 𝕜 (Fin n)),
+    T (xs.repr.symm w) = xs.repr.symm (fun i => as i * w i) by
+      simpa only [LinearIsometryEquiv.symm_apply_apply,
+        LinearIsometryEquiv.apply_symm_apply]
+        using congr_arg (fun (v : E) => (xs.repr) v i) (hsuff ((xs.repr) v))
   intros w
-  simp_rw [← OrthonormalBasis.sum_repr_symm, map_sum,
-    LinearMap.map_smul, fun j => Module.End.mem_eigenspace_iff.mp (hxs j).1,
-    smul_smul, mul_comm]
+  simp_rw [← OrthonormalBasis.sum_repr_symm, map_sum, LinearMap.map_smul,
+    fun j => Module.End.mem_eigenspace_iff.mp (hxs j).1, smul_smul, mul_comm]
 
 end LinearMap
